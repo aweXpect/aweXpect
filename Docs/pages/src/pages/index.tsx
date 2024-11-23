2fe72f2f573cc372d77dbe5c0b611a60f2e2f59d
Valentin <valentin.breuss@baur.eu>
2024-11-23 22:17:04 +0100

    Redo

--- Docs/pages/src/pages/index.tsx
+++ Docs/pages/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            aweXpect Tutorial - 5min ⏱️
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
