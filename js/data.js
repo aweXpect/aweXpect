df1ced3286d0d7dc71a9ee1b4471f815ff3b6c0b
vbreuss <vbreuss@users.noreply.github.com>
2024-11-24 12:50:44 +0000

    deploy: 3c62df724ebbb52fbc9874f38875e01b2ed286eb

---
+++ js/data.js
@@ -0,0 +1,64 @@
+window.BENCHMARK_DATA = {
+	"lastUpdate": 1732451046398,
+	"repoUrl": "https://github.com/aweXpect/aweXpect",
+	"entries": {
+		"Benchmark.Net Benchmark": [
+			{
+				"commit": {
+					"author": {
+						"name": "aweXpect",
+						"username": "aweXpect"
+					},
+					"committer": {
+						"name": "aweXpect",
+						"username": "aweXpect"
+					},
+					"id": "087a9d7cc35ef61ee0bf86be3d008c5cd444f90a",
+					"message": "fix: deployment of GitHub pages",
+					"timestamp": "2024-11-24T07:07:58Z",
+					"url": "https://github.com/aweXpect/aweXpect/pull/18/commits/087a9d7cc35ef61ee0bf86be3d008c5cd444f90a"
+				},
+				"date": 1732451046172,
+				"tool": "benchmarkdotnet",
+				"benches": [
+					{
+						"name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_FluentAssertions",
+						"value": 224.51341882546743,
+						"unit": "ns",
+						"range": "± 1.843727701198027"
+					},
+					{
+						"name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_aweXpect",
+						"value": 168.71270225842792,
+						"unit": "ns",
+						"range": "± 0.6531414309737593"
+					},
+					{
+						"name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_TUnit",
+						"value": 631.3338410513742,
+						"unit": "ns",
+						"range": "± 2.1479594622149567"
+					},
+					{
+						"name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_FluentAssertions",
+						"value": 393.7382577896118,
+						"unit": "ns",
+						"range": "± 2.4878681085251855"
+					},
+					{
+						"name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_aweXpect",
+						"value": 315.77155006848847,
+						"unit": "ns",
+						"range": "± 1.7719340159400114"
+					},
+					{
+						"name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_TUnit",
+						"value": 776.9469863451444,
+						"unit": "ns",
+						"range": "± 5.794657013041029"
+					}
+				]
+			}
+		]
+	}
+}
