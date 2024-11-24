cd443d71bf95dcc8d8a69613412fb1bfbd960a6e
Valentin <valentin.breuss@baur.eu>
2024-11-24 17:33:26 +0100

    Review comment

--- Docs/pages/src/pages/benchmarks.js
+++ Docs/pages/src/pages/benchmarks.js
@@ -4,13 +4,9 @@ import Layout from '@theme/Layout';
 export default function Benchmarks() {
 	useEffect(() => {
 		const chartScript = document.createElement('script');
-		chartScript.setAttribute('id', "benchmark-chart-script");
-		chartScript.setAttribute('async', 'true');
 		chartScript.setAttribute('src', 'js/Chart.min.js');
 		document.getElementById("__docusaurus").appendChild(chartScript);
 		const dataScript = document.createElement('script');
-		dataScript.setAttribute('id', "benchmark-data-script");
-		chartScript.setAttribute('async', 'true');
 		dataScript.setAttribute('src', 'js/data.js');
 		document.getElementById("__docusaurus").appendChild(dataScript);
 		const benchmarkScript = document.createElement('script');
