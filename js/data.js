20a07c57178a37bfda3b92ee2b1ec3d3be0f4829
vbreuss <vbreuss@users.noreply.github.com>
2024-11-25 10:07:40 +0000

    deploy: ea4c5986b91a1a1144d9b5e22c3ba50ebb7c153f

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732460915194,
+  "lastUpdate": 1732527102109,
   "repoUrl": "https://github.com/aweXpect/aweXpect",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -116,6 +116,366 @@ window.BENCHMARK_DATA = {
             "value": 836.1589536031087,
             "unit": "ns",
             "range": "± 2.790730139831681"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vbreuss@gmail.com",
+            "name": "Valentin Breuß",
+            "username": "vbreuss"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a1983143bc3e071fff853bf31380d3c67c65e4ed",
+          "message": "fix: push created benchmarks branch (#25)\n\n* Push created benchmarks branch\r\n* Rename \"Tutorial\" to \"Documentation\"",
+          "timestamp": "2024-11-24T17:31:56Z",
+          "tree_id": "9b816f1f2771f66e8ff7c48bd97ef6dea4511b97",
+          "url": "https://github.com/aweXpect/aweXpect/commit/a1983143bc3e071fff853bf31380d3c67c65e4ed"
+        },
+        "date": 1732469749027,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_FluentAssertions",
+            "value": 213.6469726392201,
+            "unit": "ns",
+            "range": "± 0.9295504907126708"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_aweXpect",
+            "value": 165.61433986255102,
+            "unit": "ns",
+            "range": "± 1.3220577808425418"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_TUnit",
+            "value": 578.4845448176067,
+            "unit": "ns",
+            "range": "± 3.190269230315376"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_FluentAssertions",
+            "value": 384.4197207768758,
+            "unit": "ns",
+            "range": "± 1.7554609275201174"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_aweXpect",
+            "value": 312.1436559132167,
+            "unit": "ns",
+            "range": "± 1.2036998548245315"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_TUnit",
+            "value": 782.8100941340128,
+            "unit": "ns",
+            "range": "± 2.17704789645261"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vbreuss@gmail.com",
+            "name": "Valentin Breuß",
+            "username": "vbreuss"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9faaf90b0f28d1e38a374d8867a54563fd849506",
+          "message": "feat: enable dependabot (#27)\n\n* Create dependabot.yml:\r\nEnable [Dependabot](https://github.com/dependabot) for nuget packages and github actions",
+          "timestamp": "2024-11-25T07:10:29Z",
+          "tree_id": "c862a8c75e2d735bd9b7fb6c4db76857e6da91bb",
+          "url": "https://github.com/aweXpect/aweXpect/commit/9faaf90b0f28d1e38a374d8867a54563fd849506"
+        },
+        "date": 1732518826919,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_FluentAssertions",
+            "value": 214.28925439289637,
+            "unit": "ns",
+            "range": "± 1.0402784443979822"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_aweXpect",
+            "value": 180.58834184919084,
+            "unit": "ns",
+            "range": "± 1.1312639456974858"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_TUnit",
+            "value": 636.6036241395133,
+            "unit": "ns",
+            "range": "± 2.4800582355124905"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_FluentAssertions",
+            "value": 398.00122276941937,
+            "unit": "ns",
+            "range": "± 2.7222887200364774"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_aweXpect",
+            "value": 322.94945297241213,
+            "unit": "ns",
+            "range": "± 2.022357264065618"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_TUnit",
+            "value": 804.0389312108358,
+            "unit": "ns",
+            "range": "± 7.259211297417814"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vbreuss@gmail.com",
+            "name": "Valentin Breuß",
+            "username": "vbreuss"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7757917bcdf48603164b537f0f5a0109e1255a64",
+          "message": "refactor: disable some jobs for dependabot pull requests (#35)\n\nDisable some jobs for dependabot, because of\r\nhttps://github.com/dependabot/dependabot-core/issues/3253",
+          "timestamp": "2024-11-25T08:56:25Z",
+          "tree_id": "c28269a0b07d63286d1fe612f26002fcb33c624c",
+          "url": "https://github.com/aweXpect/aweXpect/commit/7757917bcdf48603164b537f0f5a0109e1255a64"
+        },
+        "date": 1732525177181,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_FluentAssertions",
+            "value": 224.24265851424292,
+            "unit": "ns",
+            "range": "± 0.8798749923249037"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_aweXpect",
+            "value": 175.00747151374816,
+            "unit": "ns",
+            "range": "± 1.0865913667505251"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_TUnit",
+            "value": 629.3752957661947,
+            "unit": "ns",
+            "range": "± 2.9298772431204245"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_FluentAssertions",
+            "value": 419.8269890638498,
+            "unit": "ns",
+            "range": "± 1.752325068608801"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_aweXpect",
+            "value": 334.6943126678467,
+            "unit": "ns",
+            "range": "± 2.015542282302121"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_TUnit",
+            "value": 810.94676392419,
+            "unit": "ns",
+            "range": "± 7.474848757723167"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6e766e0bf8b7cf105b8d027796bd44c7cc222bbf",
+          "message": "build(deps): bump TUnit from 0.3.34 to 0.4.1 (#28)\n\nBumps [TUnit](https://github.com/thomhurst/TUnit) from 0.3.34 to 0.4.1.\r\n- [Commits](https://github.com/thomhurst/TUnit/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: TUnit\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-11-25T10:11:34+01:00",
+          "tree_id": "85aaf0e5b72355799b792ffc916348c40b9da71a",
+          "url": "https://github.com/aweXpect/aweXpect/commit/6e766e0bf8b7cf105b8d027796bd44c7cc222bbf"
+        },
+        "date": 1732526073805,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_FluentAssertions",
+            "value": 218.67978074000433,
+            "unit": "ns",
+            "range": "± 0.8984901638543007"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_aweXpect",
+            "value": 166.61433245454515,
+            "unit": "ns",
+            "range": "± 0.8394776132472785"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_TUnit",
+            "value": 599.6205128261021,
+            "unit": "ns",
+            "range": "± 2.193008946145856"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_FluentAssertions",
+            "value": 398.53275241851804,
+            "unit": "ns",
+            "range": "± 1.967864692301089"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_aweXpect",
+            "value": 311.9279345732469,
+            "unit": "ns",
+            "range": "± 1.642635679429469"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_TUnit",
+            "value": 872.9029039655413,
+            "unit": "ns",
+            "range": "± 2.719606059155758"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b345f7b91cafa033d92d3f02ed56b001deb8f970",
+          "message": "build(deps): bump Microsoft.Testing.Extensions.CodeCoverage from 17.12.4 to 17.12.6 (#29)\n\nbuild(deps): bump Microsoft.Testing.Extensions.CodeCoverage\r\n\r\nBumps [Microsoft.Testing.Extensions.CodeCoverage](https://github.com/microsoft/codecoverage) from 17.12.4 to 17.12.6.\r\n- [Commits](https://github.com/microsoft/codecoverage/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: Microsoft.Testing.Extensions.CodeCoverage\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-11-25T10:20:23+01:00",
+          "tree_id": "33071e5d9effa4ebc7b68ad39bc61618196925ed",
+          "url": "https://github.com/aweXpect/aweXpect/commit/b345f7b91cafa033d92d3f02ed56b001deb8f970"
+        },
+        "date": 1732526610986,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_FluentAssertions",
+            "value": 218.3828762429101,
+            "unit": "ns",
+            "range": "± 0.8676523572006164"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_aweXpect",
+            "value": 170.42119640963418,
+            "unit": "ns",
+            "range": "± 1.1002572718911776"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_TUnit",
+            "value": 616.6607441902161,
+            "unit": "ns",
+            "range": "± 2.0421279874845535"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_FluentAssertions",
+            "value": 406.7946391105652,
+            "unit": "ns",
+            "range": "± 1.7039585905613563"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_aweXpect",
+            "value": 322.47511231104534,
+            "unit": "ns",
+            "range": "± 1.1901343811866958"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_TUnit",
+            "value": 807.4594235787025,
+            "unit": "ns",
+            "range": "± 2.076231477134238"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "00f58b60401f52b94259735510f6c88f10d4b5d6",
+          "message": "build(deps): bump Microsoft.Testing.Extensions.TrxReport from 1.4.1 to 1.4.3 (#32)\n\nbuild(deps): bump Microsoft.Testing.Extensions.TrxReport\r\n\r\nBumps [Microsoft.Testing.Extensions.TrxReport](https://github.com/microsoft/testfx) from 1.4.1 to 1.4.3.\r\n- [Release notes](https://github.com/microsoft/testfx/releases)\r\n- [Changelog](https://github.com/microsoft/testfx/blob/main/docs/Changelog-Platform.md)\r\n- [Commits](https://github.com/microsoft/testfx/commits)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: Microsoft.Testing.Extensions.TrxReport\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-11-25T10:28:37+01:00",
+          "tree_id": "f62d2d2bed8df69f2ff8fcd0149d8f7d1eb36eae",
+          "url": "https://github.com/aweXpect/aweXpect/commit/00f58b60401f52b94259735510f6c88f10d4b5d6"
+        },
+        "date": 1732527101510,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_FluentAssertions",
+            "value": 219.41507858435313,
+            "unit": "ns",
+            "range": "± 1.0840968246655964"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_aweXpect",
+            "value": 169.91630367132333,
+            "unit": "ns",
+            "range": "± 0.680654803793345"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.Bool_TUnit",
+            "value": 603.7741659709385,
+            "unit": "ns",
+            "range": "± 4.838823105341517"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_FluentAssertions",
+            "value": 408.6883493423462,
+            "unit": "ns",
+            "range": "± 3.2666011382219837"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_aweXpect",
+            "value": 327.3907864570618,
+            "unit": "ns",
+            "range": "± 2.923380537155745"
+          },
+          {
+            "name": "aweXpect.Benchmarks.HappyCaseBenchmarks.String_TUnit",
+            "value": 864.404640452067,
+            "unit": "ns",
+            "range": "± 4.24399434827037"
           }
         ]
       }
