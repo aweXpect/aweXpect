58481209da777dcd337ff877324557e132279790
Valentin <valentin.breuss@baur.eu>
2024-12-06 19:47:17 +0100

    Separate benchmarks in multiple files

---
+++ Benchmarks/aweXpect.Benchmarks/HappyCaseBenchmarks.Bool.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
+namespace aweXpect.Benchmarks;
+
+public partial class HappyCaseBenchmarks
+{
+	private readonly bool _boolSubject = true;
+
+	[Benchmark]
+	public AndConstraint<BooleanAssertions> Bool_FluentAssertions()
+		=> _boolSubject.Should().BeTrue();
+
+	[Benchmark]
+	public async Task<bool> Bool_aweXpect()
+		=> await Expect.That(_boolSubject).Should().BeTrue();
+
+	[Benchmark]
+	public async Task<bool> Bool_TUnit()
+		=> await Assert.That(_boolSubject).IsTrue();
+}
