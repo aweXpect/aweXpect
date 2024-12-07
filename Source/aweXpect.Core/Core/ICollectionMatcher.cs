673cbbf2e9890b2656abf3dcf629997bbf2ed4e6
Valentin <valentin.breuss@baur.eu>
2024-12-07 14:20:49 +0100

    Review comments

--- Source/aweXpect.Core/Core/ICollectionMatcher.cs
+++ Source/aweXpect.Core/Core/ICollectionMatcher.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace aweXpect.Core;
 
 /// <summary>
@@ -10,12 +8,20 @@ public interface ICollectionMatcher<in T, out T2> where T : T2
 	/// <summary>
 	///     Verifies for each <paramref name="value" /> in the subject, if it results in a failure.
 	/// </summary>
-	/// <returns><see langword="null" /> when it results in no failure, the failure text otherwise.</returns>
+	/// <remarks>
+	///     When returning true and the <paramref name="error" /> is <see langword="null" /> this indicates, that there are too
+	///     many deviations.
+	/// </remarks>
+	/// <returns><see langword="true" /> when it results in a failure, otherwise <see langword="false" />.</returns>
 	bool Verify(string it, T value, IOptionsEquality<T2> options, out string? error);
 
 	/// <summary>
 	///     Verifies if it results in a failure when the enumeration is complete.
 	/// </summary>
-	/// <returns><see langword="null" /> when it results in no failure, the failure text otherwise.</returns>
+	/// <remarks>
+	///     When returning true and the <paramref name="error" /> is <see langword="null" /> this indicates, that there are too
+	///     many deviations.
+	/// </remarks>
+	/// <returns><see langword="true" /> when it results in a failure, otherwise <see langword="false" />.</returns>
 	bool VerifyComplete(string it, IOptionsEquality<T2> options, out string? error);
 }
