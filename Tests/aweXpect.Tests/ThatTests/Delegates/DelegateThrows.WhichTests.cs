04d74f2adf1de488e208d1ec9cd1bf462a0410ce
Valentin Breuß <vbreuss@gmail.com>
2024-12-08 10:53:46 +0100

    feat: add `.Which` for delegates (#92)
    Add `.Which` extension for delegates, that allow adding expectations for arbitrary members:
    
    ```csharp
    var exception = // Some exception...
    
    await Expect.That(() => throw exception).Should().ThrowException()
      .Which(e => e.HResult, h => h.Should().BeGreaterThan(12340))
      .Because("you can verify arbitrary additional members");
    ```

---
+++ Tests/aweXpect.Tests/ThatTests/Delegates/DelegateThrows.WhichTests.cs
@@ -0,0 +1,41 @@
+﻿using aweXpect.Tests.TestHelpers;
+
+namespace aweXpect.Tests.ThatTests.Delegates;
+
+public sealed partial class DelegateThrows
+{
+	public class WhichTests
+	{
+		[Theory]
+		[AutoData]
+		public async Task WhenMemberMatchesExpected_ShouldSucceed(int hResult)
+		{
+			Exception exception = new HResultException(hResult);
+
+			async Task Act()
+				=> await That(() => throw exception).Should().ThrowException()
+					.Which(e => e.HResult, h => h.Should().Be(hResult));
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[AutoData]
+		public async Task WhenMemberIsDifferent_ShouldFail(int hResult)
+		{
+			int expectedHResult = hResult + 1;
+			Exception exception = new HResultException(hResult);
+
+			async Task Act()
+				=> await That(() => throw exception).Should().ThrowException()
+					.Which(e => e.HResult, h => h.Should().Be(expectedHResult));
+
+			await That(Act).Should().Throw<XunitException>()
+				.WithMessage($"""
+				              Expected () => throw exception to
+				              throw an Exception which .HResult should be equal to {expectedHResult},
+				              but .HResult was {hResult}
+				              """);
+		}
+	}
+}
