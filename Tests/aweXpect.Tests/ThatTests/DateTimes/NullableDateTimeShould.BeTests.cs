79b8cf605ed058c8e488dedfc1a54012922559c0
Valentin <valentin.breuss@baur.eu>
2024-12-13 09:33:25 +0100

    fix: allow nullable `DateTime` with "Unspecified" kind to be equal to "Local" and "Universal" `DateTime`

--- Tests/aweXpect.Tests/ThatTests/DateTimes/NullableDateTimeShould.BeTests.cs
+++ Tests/aweXpect.Tests/ThatTests/DateTimes/NullableDateTimeShould.BeTests.cs
@@ -4,6 +4,22 @@ public sealed partial class NullableDateTimeShould
 {
 	public sealed class BeTests
 	{
+		[Theory]
+		[InlineData(DateTimeKind.Local)]
+		[InlineData(DateTimeKind.Utc)]
+		[InlineData(DateTimeKind.Unspecified)]
+		public async Task WhenExpectedKindIsUnspecified_ShouldSucceed(DateTimeKind subjectKind)
+		{
+			DateTime? subject = new(2024, 11, 1, 14, 15, 0, subjectKind);
+			DateTime? expected = new(2024, 11, 1, 14, 15, 0, DateTimeKind.Unspecified);
+
+			async Task Act()
+				=> await That(subject).Should().Be(expected)
+					.Because("the subject has Unspecified kind");
+
+			await That(Act).Should().NotThrow();
+		}
+
 		[Fact]
 		public async Task WhenNullableSubjectIsTheExpectedValue_ShouldSucceed()
 		{
@@ -24,6 +40,22 @@ public sealed partial class NullableDateTimeShould
 
 			async Task Act()
 				=> await That(subject).Should().Be(expected);
+
+			await That(Act).Should().NotThrow();
+		}
+
+		[Theory]
+		[InlineData(DateTimeKind.Local)]
+		[InlineData(DateTimeKind.Utc)]
+		[InlineData(DateTimeKind.Unspecified)]
+		public async Task WhenNullableSubjectKindIsUnspecified_ShouldSucceed(DateTimeKind expectedKind)
+		{
+			DateTime? subject = new(2024, 11, 1, 14, 15, 0, DateTimeKind.Unspecified);
+			DateTime? expected = new(2024, 11, 1, 14, 15, 0, expectedKind);
+
+			async Task Act()
+				=> await That(subject).Should().Be(expected)
+					.Because("the subject has Unspecified kind");
 
 			await That(Act).Should().NotThrow();
 		}
