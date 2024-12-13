d231e4bff60a4f42bdab1cf215c530b2e4d816a3
Valentin <valentin.breuss@baur.eu>
2024-12-13 14:13:36 +0100

    Speedup count items for `ICollection`

---
+++ Benchmarks/aweXpect.Benchmarks/HappyCaseBenchmarks.Collection.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using FluentAssertions;
+using FluentAssertions.Collections;
+
+namespace aweXpect.Benchmarks;
+
+public partial class HappyCaseBenchmarks
+{
+	private readonly int _enumerableCount = 1000;
+	private readonly IEnumerable<int> _enumerableSubject = Enumerable.Range(1, 1000);
+
+	[Benchmark]
+	public async Task ItemsCount_AtLeast_aweXpect()
+		=> await Expect.That(_enumerableSubject).Should().HaveAtLeast(_enumerableCount).Items();
+
+	[Benchmark]
+	public AndConstraint<GenericCollectionAssertions<int>> ItemsCount_AtLeast_FluentAssertions()
+		=> _enumerableSubject.Should().HaveCountGreaterThanOrEqualTo(_enumerableCount);
+
+	[Benchmark]
+	public async Task ItemsCount_AtLeast_TUnit()
+		=> await Assert.That(_enumerableSubject).HasCount().GreaterThanOrEqualTo(_enumerableCount);
+}
---
+++ Benchmarks/aweXpect.Benchmarks/HappyCaseBenchmarks.Int.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using FluentAssertions;
+using FluentAssertions.Numeric;
+
+namespace aweXpect.Benchmarks;
+
+public partial class HappyCaseBenchmarks
+{
+	private readonly int _intSubject = 42;
+	private readonly int _intMinimum = 20;
+
+	[Benchmark]
+	public async Task<int> Int_GreaterThan_aweXpect()
+		=> await Expect.That(_intSubject).Should().BeGreaterThan(_intMinimum);
+
+	[Benchmark]
+	public AndConstraint<NumericAssertions<int>> Int_GreaterThan_FluentAssertions()
+		=> _intSubject.Should().BeGreaterThan(_intMinimum);
+
+	[Benchmark]
+	public async Task<int> Int_GreaterThan_TUnit()
+		=> await Assert.That(_intSubject).IsGreaterThan(_intMinimum);
+}
