a164743ffd3a197b1836fa5e7bd791be950e6236
Valentin <valentin.breuss@baur.eu>
2024-12-19 12:18:26 +0100

    Add missing tests

---
+++ Tests/aweXpect.Core.Tests/Core/Sources/DelegateValueSourceTests.cs
@@ -0,0 +1,18 @@
+﻿using aweXpect.Core.Tests.TestHelpers;
+
+namespace aweXpect.Core.Tests.Core.Sources;
+
+public class DelegateValueSourceTests
+{
+	[Fact]
+	public async Task ForExecutionTime_ShouldUseElapsedFromTimeSystem()
+	{
+		TimeSystemMock timeSystem = new TimeSystemMock().SetElapsed(TimeSpan.FromSeconds(1.1));
+
+		async Task Act() =>
+			await That(() => 1).Should().NotExecuteWithin(TimeSpan.FromSeconds(1))
+				.UseTimeSystem(timeSystem);
+
+		await That(Act).Should().NotThrow();
+	}
+}
---
+++ Tests/aweXpect.Core.Tests/TestHelpers/TimeSystemMock.cs
@@ -0,0 +1,41 @@
+﻿using aweXpect.Core.TimeSystem;
+
+namespace aweXpect.Core.Tests.TestHelpers;
+
+internal class TimeSystemMock : ITimeSystem
+{
+	private readonly StopwatchFactoryMock _stopwatchFactory = new();
+	public IStopwatchFactory Stopwatch => _stopwatchFactory;
+
+	public TimeSystemMock SetElapsed(TimeSpan elapsed)
+	{
+		_stopwatchFactory.SetElapsed(elapsed);
+		return this;
+	}
+
+	private class StopwatchFactoryMock : IStopwatchFactory
+	{
+		private TimeSpan _elapsed = TimeSpan.Zero;
+		public IStopwatch New() => new StopwatchMock(() => _elapsed);
+
+		public void SetElapsed(TimeSpan elapsed) => _elapsed = elapsed;
+	}
+
+	private class StopwatchMock(Func<TimeSpan> getElapsed) : IStopwatch
+	{
+		public TimeSpan Elapsed { get; private set; }
+		public bool IsRunning { get; private set; }
+
+		public void Start() => IsRunning = true;
+
+		public void Stop()
+		{
+			if (IsRunning)
+			{
+				Elapsed = getElapsed();
+			}
+
+			IsRunning = false;
+		}
+	}
+}
