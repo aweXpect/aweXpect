29c087a7d3dfc6fb90ea9209f0f732dd9d9df4bd
Valentin Breuß <vbreuss@gmail.com>
2024-12-20 17:09:36 +0100

    feat: add `StartWith` collection extensions (#144)
    You can verify, that a collection starts with another collection:
    ```csharp
    IEnumerable<int> values = Enumerable.Range(1, 3);
    
    await Expect.That(values).Should().StartWith(1, 2);
    ```
    
    You can also use a [custom comparer](/docs/expectations/object#custom-comparer) or configure [equivalence](/docs/expectations/object#equivalence):
    ```csharp
    IEnumerable<MyClass> values = //...
    MyClass expected = //...
    await Expect.That(values).Should().StartWith(expected).Equivalent();
    await Expect.That(values).Should().StartWith(expected).Using(new MyClassComparer());
    ```
    
    For strings, you can configure this expectation to ignore case, ignore newline style, ignoring leading or trailing white-space, or use a custom `IEqualityComparer<string>`:
    ```csharp
    await Expect.That(["FOO", "BAR"]).Should().StartWith(["foo"]).IgnoringCase();
    ```
    
    *Note: The same expectation works also for `IAsyncEnumerable<T>`.*

---
+++ Source/aweXpect/That/Collections/ThatAsyncEnumerableShould.StartWith.cs
@@ -0,0 +1,155 @@
+﻿#if NET8_0_OR_GREATER
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using aweXpect.Core;
+using aweXpect.Core.Constraints;
+using aweXpect.Core.EvaluationContext;
+using aweXpect.Helpers;
+using aweXpect.Options;
+using aweXpect.Results;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace aweXpect;
+
+public static partial class ThatAsyncEnumerableShould
+{
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static ObjectEqualityResult<IAsyncEnumerable<TItem>, IThat<IAsyncEnumerable<TItem>>>
+		StartWith<TItem>(
+			this IThat<IAsyncEnumerable<TItem>> source,
+			IEnumerable<TItem> expected,
+			[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
+	{
+		ObjectEqualityOptions options = new();
+		return new ObjectEqualityResult<IAsyncEnumerable<TItem>, IThat<IAsyncEnumerable<TItem>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<TItem, object?>(it, doNotPopulateThisValue, expected.ToArray(),
+						options)),
+			source,
+			options);
+	}
+
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static ObjectEqualityResult<IAsyncEnumerable<TItem>, IThat<IAsyncEnumerable<TItem>>>
+		StartWith<TItem>(
+			this IThat<IAsyncEnumerable<TItem>> source,
+			params TItem[] expected)
+	{
+		ObjectEqualityOptions options = new();
+		return new ObjectEqualityResult<IAsyncEnumerable<TItem>, IThat<IAsyncEnumerable<TItem>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<TItem, object?>(it, Formatter.Format(expected), expected, options)),
+			source,
+			options);
+	}
+
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static StringEqualityResult<IAsyncEnumerable<string>, IThat<IAsyncEnumerable<string>>>
+		StartWith(
+			this IThat<IAsyncEnumerable<string>> source,
+			IEnumerable<string> expected,
+			[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
+	{
+		StringEqualityOptions options = new();
+		return new StringEqualityResult<IAsyncEnumerable<string>, IThat<IAsyncEnumerable<string>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<string, string>(it, doNotPopulateThisValue, expected.ToArray(),
+						options)),
+			source,
+			options);
+	}
+
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static StringEqualityResult<IAsyncEnumerable<string>, IThat<IAsyncEnumerable<string>>>
+		StartWith(
+			this IThat<IAsyncEnumerable<string>> source,
+			params string[] expected)
+	{
+		StringEqualityOptions options = new();
+		return new StringEqualityResult<IAsyncEnumerable<string>, IThat<IAsyncEnumerable<string>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<string, string>(it, Formatter.Format(expected), expected, options)),
+			source,
+			options);
+	}
+
+	private readonly struct StartWithConstraint<TItem, TMatch> : IAsyncContextConstraint<IAsyncEnumerable<TItem>>
+		where TItem : TMatch
+	{
+		private readonly string _it;
+		private readonly string _expectedExpression;
+		private readonly TItem[] _expected;
+		private readonly IOptionsEquality<TMatch> _options;
+
+		public StartWithConstraint(string it,
+			string expectedExpression,
+			TItem[] expected,
+			IOptionsEquality<TMatch> options)
+		{
+			ArgumentNullException.ThrowIfNull(expected);
+			_it = it;
+			_expectedExpression = expectedExpression;
+			_expected = expected;
+			_options = options;
+		}
+
+		public async Task<ConstraintResult> IsMetBy(IAsyncEnumerable<TItem> actual, IEvaluationContext context,
+			CancellationToken cancellationToken)
+		{
+			// ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+			if (actual is null)
+			{
+				return new ConstraintResult.Failure(ToString(),
+					$"{_it} was <null>");
+			}
+
+			IAsyncEnumerable<TItem> materializedEnumerable =
+				context.UseMaterializedAsyncEnumerable<TItem, IAsyncEnumerable<TItem>>(actual);
+			if (_expected.Length == 0)
+			{
+				return new ConstraintResult.Success<IAsyncEnumerable<TItem>>(actual, ToString());
+			}
+
+			int index = 0;
+			await foreach (TItem item in materializedEnumerable.WithCancellation(cancellationToken))
+			{
+				TItem expectedItem = _expected[index++];
+				if (!_options.AreConsideredEqual(item, expectedItem))
+				{
+					return new ConstraintResult.Failure<IAsyncEnumerable<TItem>>(actual, ToString(),
+						$"{_it} contained {Formatter.Format(item)} at index {index - 1} instead of {Formatter.Format(expectedItem)}");
+				}
+
+				if (_expected.Length == index)
+				{
+					return new ConstraintResult.Success<IAsyncEnumerable<TItem>>(actual, ToString());
+				}
+			}
+
+
+			return new ConstraintResult.Failure<IAsyncEnumerable<TItem>>(actual, ToString(),
+				$"{_it} contained only {index} items and misses {_expected.Length - index} items: {Formatter.Format(_expected.Skip(index), FormattingOptions.MultipleLines)}");
+		}
+
+		public override string ToString()
+			=> $"start with {_expectedExpression}";
+	}
+}
+#endif
---
+++ Source/aweXpect/That/Collections/ThatEnumerableShould.StartWith.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using aweXpect.Core;
+using aweXpect.Core.Constraints;
+using aweXpect.Core.EvaluationContext;
+using aweXpect.Helpers;
+using aweXpect.Options;
+using aweXpect.Results;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace aweXpect;
+
+public static partial class ThatEnumerableShould
+{
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static ObjectEqualityResult<IEnumerable<TItem>, IThat<IEnumerable<TItem>>>
+		StartWith<TItem>(
+			this IThat<IEnumerable<TItem>> source,
+			IEnumerable<TItem> expected,
+			[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
+	{
+		ObjectEqualityOptions options = new();
+		return new ObjectEqualityResult<IEnumerable<TItem>, IThat<IEnumerable<TItem>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<TItem, object?>(it, doNotPopulateThisValue, expected.ToArray(),
+						options)),
+			source,
+			options);
+	}
+
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static ObjectEqualityResult<IEnumerable<TItem>, IThat<IEnumerable<TItem>>>
+		StartWith<TItem>(
+			this IThat<IEnumerable<TItem>> source,
+			params TItem[] expected)
+	{
+		ObjectEqualityOptions options = new();
+		return new ObjectEqualityResult<IEnumerable<TItem>, IThat<IEnumerable<TItem>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<TItem, object?>(it, Formatter.Format(expected), expected, options)),
+			source,
+			options);
+	}
+
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static StringEqualityResult<IEnumerable<string>, IThat<IEnumerable<string>>>
+		StartWith(
+			this IThat<IEnumerable<string>> source,
+			IEnumerable<string> expected,
+			[CallerArgumentExpression("expected")] string doNotPopulateThisValue = "")
+	{
+		StringEqualityOptions options = new();
+		return new StringEqualityResult<IEnumerable<string>, IThat<IEnumerable<string>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<string, string>(it, doNotPopulateThisValue, expected.ToArray(),
+						options)),
+			source,
+			options);
+	}
+
+	/// <summary>
+	///     Verifies that the collection starts with the provided <paramref name="expected" /> collection.
+	/// </summary>
+	public static StringEqualityResult<IEnumerable<string>, IThat<IEnumerable<string>>>
+		StartWith(
+			this IThat<IEnumerable<string>> source,
+			params string[] expected)
+	{
+		StringEqualityOptions options = new();
+		return new StringEqualityResult<IEnumerable<string>, IThat<IEnumerable<string>>>(source
+				.ExpectationBuilder
+				.AddConstraint(it
+					=> new StartWithConstraint<string, string>(it, Formatter.Format(expected), expected, options)),
+			source,
+			options);
+	}
+
+	private readonly struct StartWithConstraint<TItem, TMatch> : IContextConstraint<IEnumerable<TItem>>
+		where TItem : TMatch
+	{
+		private readonly string _it;
+		private readonly string _expectedExpression;
+		private readonly TItem[] _expected;
+		private readonly IOptionsEquality<TMatch> _options;
+
+		public StartWithConstraint(string it,
+			string expectedExpression,
+			TItem[] expected,
+			IOptionsEquality<TMatch> options)
+		{
+			_it = it;
+			_expectedExpression = expectedExpression;
+			_expected = expected ?? throw new ArgumentNullException(nameof(expected));
+			_options = options;
+		}
+
+		public ConstraintResult IsMetBy(IEnumerable<TItem> actual, IEvaluationContext context)
+		{
+			// ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+			if (actual is null)
+			{
+				return new ConstraintResult.Failure(ToString(),
+					$"{_it} was <null>");
+			}
+
+			IEnumerable<TItem> materializedEnumerable =
+				context.UseMaterializedEnumerable<TItem, IEnumerable<TItem>>(actual);
+			if (_expected.Length == 0)
+			{
+				return new ConstraintResult.Success<IEnumerable<TItem>>(actual, ToString());
+			}
+
+			int index = 0;
+			foreach (TItem item in materializedEnumerable)
+			{
+				TItem expectedItem = _expected[index++];
+				if (!_options.AreConsideredEqual(item, expectedItem))
+				{
+					return new ConstraintResult.Failure<IEnumerable<TItem>>(actual, ToString(),
+						$"{_it} contained {Formatter.Format(item)} at index {index - 1} instead of {Formatter.Format(expectedItem)}");
+				}
+
+				if (_expected.Length == index)
+				{
+					return new ConstraintResult.Success<IEnumerable<TItem>>(actual, ToString());
+				}
+			}
+
+
+			return new ConstraintResult.Failure<IEnumerable<TItem>>(actual, ToString(),
+				$"{_it} contained only {index} items and misses {_expected.Length - index} items: {Formatter.Format(_expected.Skip(index), FormattingOptions.MultipleLines)}");
+		}
+
+		public override string ToString()
+			=> $"start with {_expectedExpression}";
+	}
+}
