30f0779109dbd0ff7f94dc8dcd7704c2ced84f39
Valentin <valentin.breuss@baur.eu>
2024-12-29 18:25:06 +0100

    feat: allow specifying the default timeout for the `SignalCounter`

---
+++ Source/aweXpect.Core/Customization/Customize.Recording.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace aweXpect.Customization;
+
+public partial class Customize : ICustomizeRecording
+{
+	private static readonly int DefaultTimeoutSeconds = 30;
+
+	private readonly AsyncLocal<TimeSpan?> _defaultTimeout = new();
+
+	/// <summary>
+	///     Customizes the recording settings.
+	/// </summary>
+	public static ICustomizeRecording Recording => Instance;
+
+	/// <inheritdoc />
+	TimeSpan ICustomizeRecording.DefaultTimeout
+		=> _defaultTimeout.Value ?? TimeSpan.FromSeconds(DefaultTimeoutSeconds);
+
+	/// <inheritdoc />
+	IDisposable ICustomizeRecording.SetDefaultTimeout(TimeSpan timeout)
+	{
+		_defaultTimeout.Value = timeout;
+		return new ActionDisposable(() => _defaultTimeout.Value = null);
+	}
+}
---
+++ Source/aweXpect.Core/Customization/ICustomizeRecording.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using aweXpect.Recording;
+
+namespace aweXpect.Customization;
+
+/// <summary>
+///     Customizes the recording settings.
+/// </summary>
+public interface ICustomizeRecording
+{
+	/// <summary>
+	///     The default timeout for the <see cref="SignalCounter" />.
+	/// </summary>
+	TimeSpan DefaultTimeout { get; }
+
+	/// <summary>
+	///     Specifies the default timeout for the <see cref="SignalCounter" />.
+	/// </summary>
+	/// <returns>
+	///     An object, that will revert the default timeout to 30 seconds upon disposal.
+	/// </returns>
+	IDisposable SetDefaultTimeout(TimeSpan timeout);
+}
