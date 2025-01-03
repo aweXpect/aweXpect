561965e001df0830d20b6fa6db861fba7ca925f6
Valentin <valentin.breuss@baur.eu>
2025-01-03 21:08:05 +0100

    Improve BecauseReason coverage

---
+++ Tests/aweXpect.Core.Tests/Core/Helpers/BecauseReasonTests.cs
@@ -0,0 +1,30 @@
+﻿using aweXpect.Core.Helpers;
+
+namespace aweXpect.Core.Tests.Core.Helpers;
+
+public class BecauseReasonTests
+{
+	[Fact]
+	public async Task WhenReasonDoesNotStartWithBecause_ShouldPrefixCommaAndBecause()
+	{
+		string reason = "something";
+		string expected = ", because something";
+		BecauseReason sut = new(reason);
+
+		string result = sut.ToString();
+
+		await That(result).Should().Be(expected);
+	}
+
+	[Fact]
+	public async Task WhenReasonStartsWithBecause_ShouldPrefixComma()
+	{
+		string reason = "because something";
+		string expected = ", because something";
+		BecauseReason sut = new(reason);
+
+		string result = sut.ToString();
+
+		await That(result).Should().Be(expected);
+	}
+}
