ec1733f3987a2538366b05650bb6d2052278bd03
Valentin <valentin.breuss@baur.eu>
2025-01-03 20:38:04 +0100

    Add ConstraintResultTests.cs

---
+++ Tests/aweXpect.Core.Tests/Core/Constraints/ConstraintResultTests.cs
@@ -0,0 +1,39 @@
+﻿using aweXpect.Core.Constraints;
+
+namespace aweXpect.Core.Tests.Core.Constraints;
+
+public class ConstraintResultTests
+{
+	[Fact]
+	public async Task Success_T_TryGetValue_WhenTypeDoesNotMatch_ShouldReturnFalse()
+	{
+		ConstraintResult.Success<int> sut = new(1, "foo");
+
+		bool result = sut.TryGetValue(out string? value);
+
+		await That(result).Should().BeFalse();
+		await That(value).Should().BeNull();
+	}
+
+	[Fact]
+	public async Task Success_T_TryGetValue_WhenTypeMatches_ShouldReturnTrue()
+	{
+		ConstraintResult.Success<string> sut = new("bar", "foo");
+
+		bool result = sut.TryGetValue(out string? value);
+
+		await That(result).Should().BeTrue();
+		await That(value).Should().Be("bar");
+	}
+
+	[Fact]
+	public async Task Success_TryGetValue_ShouldReturnFalse()
+	{
+		ConstraintResult.Success sut = new("foo");
+
+		bool result = sut.TryGetValue(out string? value);
+
+		await That(result).Should().BeFalse();
+		await That(value).Should().BeNull();
+	}
+}
