9757927eccdfb89a67b4494e3b06b398176cb0c5
Valentin <valentin.breuss@baur.eu>
2025-01-18 12:04:23 +0100

    Move adapters to aweXpect.Core

---
+++ Tests/Frameworks/aweXpect.Frameworks.Fallback.Tests/AssemblySetup.cs
@@ -0,0 +1,22 @@
+﻿using aweXpect.Customization;
+using NUnit.Framework;
+
+namespace aweXpect.Frameworks.Fallback.Tests;
+
+[SetUpFixture]
+public class AssemblySetup
+{
+	private CustomizationLifetime _customizer;
+
+	[OneTimeSetUp]
+	public void RunBeforeAnyTests()
+	{
+		_customizer = Customize.aweXpect.Reflection().ExcludedAssemblyPrefixes.Set([
+			..Customize.aweXpect.Reflection().ExcludedAssemblyPrefixes.Get(),
+			"aweXpect.Core"
+		]);
+	}
+
+	[OneTimeTearDown]
+	public void RunAfterAnyTests() => _customizer.Dispose();
+}
--- Tests/Frameworks/aweXpect.Frameworks.Fallback.Tests/FallbackTests.cs
+++ Tests/Frameworks/aweXpect.Frameworks.Fallback.Tests/FallbackTests.cs
@@ -1,26 +1,27 @@
-﻿using Xunit;
+﻿
+using NUnit.Framework;
 
 namespace aweXpect.Frameworks.Fallback.Tests;
 
 public sealed class FallbackTests
 {
-	[Fact]
+	[Test]
 	public void OnFail_WhenNotSpecifyingAnyTestFramework_ShouldFallbackToThrowingAFailException()
 	{
 		void Act()
 			=> Fail.Test("my message");
 
-		FailException exception = Assert.Throws<FailException>(Act);
-		Assert.Equal("my message", exception.Message);
+		FailException? exception = Assert.Throws<FailException>(Act);
+		Assert.That(exception!.Message, Is.EqualTo("my message"));
 	}
 
-	[Fact]
+	[Test]
 	public void OnSkip_WhenNotSpecifyingAnyTestFramework_ShouldFallbackToThrowingASkipException()
 	{
 		void Act()
 			=> Skip.Test("my message");
 
-		SkipException exception = Assert.Throws<SkipException>(Act);
-		Assert.Equal("my message", exception.Message);
+		SkipException? exception = Assert.Throws<SkipException>(Act);
+		Assert.That(exception!.Message, Is.EqualTo("my message"));
 	}
 }
