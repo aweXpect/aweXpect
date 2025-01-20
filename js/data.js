6d236864e94458e2e424d1d48b5ca7620320ebbd
vbreuss <vbreuss@users.noreply.github.com>
2025-01-20 06:47:31 +0000

    deploy: 198447f7ad33650ea18d7239d6579cda44f17f2f

--- js/data.js
+++ js/data.js
@@ -1,2 +1,578 @@
 window.BENCHMARK_DATA = {
+  "Bool": {
+    "commits": [
+      {
+        "sha": "198447f7ad33650ea18d7239d6579cda44f17f2f",
+        "author": "Valentin Breu\u00DF",
+        "date": "Sun Jan 19 22:32:18 2025 \u002B0100",
+        "message": "fix: execute benchmark report in main build (#219)"
+      }
+    ],
+    "labels": [
+      "198447f7"
+    ],
+    "datasets": [
+      {
+        "label": "aweXpect time",
+        "unit": "ns",
+        "data": [
+          150.75262652910672
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "aweXpect memory",
+        "unit": "b",
+        "data": [
+          496
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "FluentAssertions time",
+        "unit": "ns",
+        "data": [
+          200.27242479721704
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "FluentAssertions memory",
+        "unit": "b",
+        "data": [
+          776
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "TUnit time",
+        "unit": "ns",
+        "data": [
+          539.4753929138184
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "TUnit memory",
+        "unit": "b",
+        "data": [
+          1712
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      }
+    ]
+  },
+  "ItemsCount_AtLeast": {
+    "commits": [
+      {
+        "sha": "198447f7ad33650ea18d7239d6579cda44f17f2f",
+        "author": "Valentin Breu\u00DF",
+        "date": "Sun Jan 19 22:32:18 2025 \u002B0100",
+        "message": "fix: execute benchmark report in main build (#219)"
+      }
+    ],
+    "labels": [
+      "198447f7"
+    ],
+    "datasets": [
+      {
+        "label": "aweXpect time",
+        "unit": "ns",
+        "data": [
+          329.63557247320813
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "aweXpect memory",
+        "unit": "b",
+        "data": [
+          888
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "FluentAssertions time",
+        "unit": "ns",
+        "data": [
+          417.0791400029109
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "FluentAssertions memory",
+        "unit": "b",
+        "data": [
+          1816
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "TUnit time",
+        "unit": "ns",
+        "data": [
+          14911.549033610027
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "TUnit memory",
+        "unit": "b",
+        "data": [
+          26616
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      }
+    ]
+  },
+  "Int_GreaterThan": {
+    "commits": [
+      {
+        "sha": "198447f7ad33650ea18d7239d6579cda44f17f2f",
+        "author": "Valentin Breu\u00DF",
+        "date": "Sun Jan 19 22:32:18 2025 \u002B0100",
+        "message": "fix: execute benchmark report in main build (#219)"
+      }
+    ],
+    "labels": [
+      "198447f7"
+    ],
+    "datasets": [
+      {
+        "label": "aweXpect time",
+        "unit": "ns",
+        "data": [
+          204.06733180681866
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "aweXpect memory",
+        "unit": "b",
+        "data": [
+          848
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "FluentAssertions time",
+        "unit": "ns",
+        "data": [
+          250.96880504063196
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "FluentAssertions memory",
+        "unit": "b",
+        "data": [
+          1048
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "TUnit time",
+        "unit": "ns",
+        "data": [
+          766.1536415735881
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "TUnit memory",
+        "unit": "b",
+        "data": [
+          2352
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      }
+    ]
+  },
+  "String": {
+    "commits": [
+      {
+        "sha": "198447f7ad33650ea18d7239d6579cda44f17f2f",
+        "author": "Valentin Breu\u00DF",
+        "date": "Sun Jan 19 22:32:18 2025 \u002B0100",
+        "message": "fix: execute benchmark report in main build (#219)"
+      }
+    ],
+    "labels": [
+      "198447f7"
+    ],
+    "datasets": [
+      {
+        "label": "aweXpect time",
+        "unit": "ns",
+        "data": [
+          304.53645614477307
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "aweXpect memory",
+        "unit": "b",
+        "data": [
+          928
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "FluentAssertions time",
+        "unit": "ns",
+        "data": [
+          351.26639202662875
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "FluentAssertions memory",
+        "unit": "b",
+        "data": [
+          1832
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "TUnit time",
+        "unit": "ns",
+        "data": [
+          814.6544300715128
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "TUnit memory",
+        "unit": "b",
+        "data": [
+          2328
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      }
+    ]
+  },
+  "StringArray": {
+    "commits": [
+      {
+        "sha": "198447f7ad33650ea18d7239d6579cda44f17f2f",
+        "author": "Valentin Breu\u00DF",
+        "date": "Sun Jan 19 22:32:18 2025 \u002B0100",
+        "message": "fix: execute benchmark report in main build (#219)"
+      }
+    ],
+    "labels": [
+      "198447f7"
+    ],
+    "datasets": [
+      {
+        "label": "aweXpect time",
+        "unit": "ns",
+        "data": [
+          1069.4012530190605
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "aweXpect memory",
+        "unit": "b",
+        "data": [
+          2384
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "FluentAssertions time",
+        "unit": "ns",
+        "data": [
+          1175.168862915039
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "FluentAssertions memory",
+        "unit": "b",
+        "data": [
+          3760
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "TUnit time",
+        "unit": "ns",
+        "data": [
+          2060.838473510742
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "TUnit memory",
+        "unit": "b",
+        "data": [
+          3216
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      }
+    ]
+  },
+  "StringArrayInAnyOrder": {
+    "commits": [
+      {
+        "sha": "198447f7ad33650ea18d7239d6579cda44f17f2f",
+        "author": "Valentin Breu\u00DF",
+        "date": "Sun Jan 19 22:32:18 2025 \u002B0100",
+        "message": "fix: execute benchmark report in main build (#219)"
+      }
+    ],
+    "labels": [
+      "198447f7"
+    ],
+    "datasets": [
+      {
+        "label": "aweXpect time",
+        "unit": "ns",
+        "data": [
+          1094.4609926768712
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "aweXpect memory",
+        "unit": "b",
+        "data": [
+          2408
+        ],
+        "borderColor": "#63A2AC",
+        "backgroundColor": "#63A2AC",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "FluentAssertions time",
+        "unit": "ns",
+        "data": [
+          76939.67994791667
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "FluentAssertions memory",
+        "unit": "b",
+        "data": [
+          50710
+        ],
+        "borderColor": "#ACA263",
+        "backgroundColor": "#ACA263",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      },
+      {
+        "label": "TUnit time",
+        "unit": "ns",
+        "data": [
+          3975.66723429362
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y",
+        "borderDash": [],
+        "pointStyle": "circle"
+      },
+      {
+        "label": "TUnit memory",
+        "unit": "b",
+        "data": [
+          5160
+        ],
+        "borderColor": "#AC6262",
+        "backgroundColor": "#AC6262",
+        "yAxisID": "y1",
+        "borderDash": [
+          5,
+          5
+        ],
+        "pointStyle": "triangle"
+      }
+    ]
+  }
 }
