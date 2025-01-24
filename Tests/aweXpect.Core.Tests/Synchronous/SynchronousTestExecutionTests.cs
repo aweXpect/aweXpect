54efc3c7d8661eeb924d484a22be7c2ddc171df9
Valentin <valentin.breuss@baur.eu>
2025-01-24 08:49:58 +0100

    Add `.Verify()` extension, that executes the expectations synchronously for use in a context where `async` is not supported (e.g. `ref struct`).

---
+++ Tests/aweXpect.Core.Tests/Synchronous/SynchronousTestExecutionTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.CompilerServices;
+using aweXpect.Core.Tests.TestHelpers;
+using aweXpect.Synchronous;
+
+namespace aweXpect.Core.Tests.Synchronous;
+
+public class SynchronousTestExecutionTests
+{
+	[Fact]
+	public void WhenActionDoesNotThrow_ShouldSucceed()
+	{
+		Foo subject = new()
+		{
+			Bar = 3
+		};
+
+		int value = subject.Bar;
+		That(() => ThrowIf(value != 3)).DoesNotThrow().Verify();
+	}
+
+	[Fact]
+	public void WhenActionThrows_ShouldFail()
+	{
+		Foo subject = new()
+		{
+			Bar = 3
+		};
+		int value = subject.Bar;
+		void Act() => That(() => ThrowIf(value == 3)).DoesNotThrow().Verify();
+
+		That(Act).Throws<XunitException>()
+			.WithMessage("""
+			             Expected () => ThrowIf(value == 3) to
+			             not throw any exception,
+			             but it did throw a MyException:
+			               WhenActionThrows_ShouldFail
+			             """).Verify();
+	}
+
+	[Fact]
+	public void WhenPropertyValuesMatch_ShouldFail()
+	{
+		Foo subject = new()
+		{
+			Bar = 3
+		};
+		int value = subject.Bar;
+		void Act() => That(value).Is(2).Verify();
+
+		That(Act).Throws<XunitException>()
+			.WithMessage("""
+			             Expected value to
+			             be equal to 2,
+			             but it was 3
+			             """).Verify();
+	}
+
+	[Fact]
+	public void WhenPropertyValuesMatch_ShouldSucceed()
+	{
+		Foo subject = new()
+		{
+			Bar = 3
+		};
+
+		int value = That(subject.Bar).Is(3).Verify();
+
+		That(value).Is(3).Verify();
+	}
+
+	private static void ThrowIf(bool condition, [CallerMemberName] string message = "")
+	{
+		if (condition)
+		{
+			throw new MyException(message);
+		}
+	}
+
+	private ref struct Foo
+	{
+		public int Bar;
+	}
+}
