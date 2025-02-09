d08b729b4bcb0b856eeaa9a10384539d839bf288
Valentin <valentin.breuss@baur.eu>
2025-02-09 14:08:12 +0100

    Consolidate test structure

---
+++ Tests/aweXpect.Tests/Objects/ThatObject.Is.WhoseTests.cs
@@ -0,0 +1,44 @@
+﻿namespace aweXpect.Tests;
+
+public sealed partial class ThatObject
+{
+	public sealed partial class Is
+	{
+		public sealed class WhoseTests
+		{
+			[Fact]
+			public async Task WhenPropertyDoesNotMatch_ShouldSucceed()
+			{
+				object subject = new MyClass
+				{
+					Value = 42
+				};
+
+				async Task Act()
+					=> await That(subject).Is<MyClass>()
+						.Whose(x => x.Value, x => x.IsLessThan(42));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is type MyClass whose .Value is less than 42,
+					             but .Value was 42
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenPropertyMatches_ShouldSucceed()
+			{
+				object subject = new MyClass
+				{
+					Value = 42
+				};
+
+				async Task Act()
+					=> await That(subject).Is<MyClass>().Whose(x => x.Value, x => x.IsEqualTo(42));
+
+				await That(Act).DoesNotThrow();
+			}
+		}
+	}
+}
