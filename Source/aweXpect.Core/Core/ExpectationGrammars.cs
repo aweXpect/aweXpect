336869fe1181dd9d0dd074b7ed15e2230dfd354a
Valentin <valentin.breuss@baur.eu>
2025-02-09 07:52:18 +0100

    Fix sonar issue

--- Source/aweXpect.Core/Core/ExpectationGrammars.cs
+++ Source/aweXpect.Core/Core/ExpectationGrammars.cs
@@ -11,20 +11,20 @@ public enum ExpectationGrammars
 	/// <summary>
 	///     The default expectation text.
 	/// </summary>
-	None,
+	None = 0,
 
 	/// <summary>
 	///     The expectation is nested.
 	/// </summary>
-	Nested,
+	Nested = 1 << 1,
 
 	/// <summary>
 	///     The expectation should be in plural form.
 	/// </summary>
-	Plural,
+	Plural = 1 << 2,
 
 	/// <summary>
 	///     The expectation should be in active voice.
 	/// </summary>
-	Active
+	Active = 1 << 3,
 }
