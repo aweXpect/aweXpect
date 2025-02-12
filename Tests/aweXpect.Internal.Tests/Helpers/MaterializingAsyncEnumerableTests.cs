23b59cb4b6304a90c69fde9833222439db4a2f3f
Valentin Breuß <vbreuss@gmail.com>
2025-02-12 21:51:50 +0100

    refactor: improve code coverage in aweXpect (#354)
    Add missing tests in aweXpect:
    - `ExpectHelpers`
    - Materializing enumerables
    - `StringExtensions`
    
    Remove unnecessary `TypeExtensions` and unnecessary methods in `StringExtensions`

---
+++ Tests/aweXpect.Internal.Tests/Helpers/MaterializingAsyncEnumerableTests.cs
@@ -0,0 +1,41 @@
+﻿#if NET8_0_OR_GREATER
+using System.Collections.Generic;
+using aweXpect.Helpers;
+
+namespace aweXpect.Internal.Tests.Helpers;
+
+public class MaterializingAsyncEnumerableTests
+{
+	[Fact]
+	public async Task WhenIterating_ShouldReturnAllValues()
+	{
+		IAsyncEnumerable<int> enumerable = ToAsyncEnumerable([1, 2, 3]);
+
+		IAsyncEnumerable<int> materialized = MaterializingAsyncEnumerable<int>.Wrap(enumerable);
+
+		await That(materialized).IsEqualTo([1, 2, 3]);
+	}
+
+	[Fact]
+	public async Task Wrap_Twice_ShouldUseSameInstance()
+	{
+		IAsyncEnumerable<int> enumerable = ToAsyncEnumerable([1, 2, 3]);
+
+		IAsyncEnumerable<int> materialized1 = MaterializingAsyncEnumerable<int>.Wrap(enumerable);
+		IAsyncEnumerable<int> materialized2 = MaterializingAsyncEnumerable<int>.Wrap(materialized1);
+
+		await That(enumerable).IsNotSameAs(materialized1);
+		await That(materialized1).IsSameAs(materialized2);
+	}
+
+
+	private static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(T[] items)
+	{
+		foreach (T item in items)
+		{
+			await Task.Yield();
+			yield return item;
+		}
+	}
+}
+#endif
