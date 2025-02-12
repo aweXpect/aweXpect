ce2a69e2ced697fa55de563b540ae7a70dd91e4f
Valentin <valentin.breuss@baur.eu>
2025-02-12 21:41:12 +0100

    refactor: improve code coverage in aweXpect
    Add missing tests in aweXpect:
    - `ExpectHelpers`
    - Materializing enumerables
    - `StringExtensions`
    
    Remove unnecessary `TypeExtensions` and unnecessary methods in `StringExtensions`

--- Source/aweXpect/Helpers/StringExtensions.cs
+++ Source/aweXpect/Helpers/StringExtensions.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace aweXpect.Helpers;
 
 internal static class StringExtensions
 {
-	[return: NotNullIfNotNull(nameof(value))]
-	public static string? DisplayWhitespace(this string? value)
-		=> value?.Replace("\n", "\\n").Replace("\r", "\\r").Replace("\t", "\\t");
-
 	[return: NotNullIfNotNull(nameof(value))]
 	public static string? Indent(this string? value, string indentation = "  ",
 		bool indentFirstLine = true)
@@ -32,61 +27,5 @@ internal static class StringExtensions
 		}
 
 		return $"a {value}";
-	}
-
-	[return: NotNullIfNotNull(nameof(value))]
-	public static string? RemoveNewlineStyle(this string? value)
-	{
-		if (value == null)
-		{
-			return null;
-		}
-
-		return value.Replace("\r\n", "\n", StringComparison.Ordinal)
-			.Replace("\r", "\n", StringComparison.Ordinal);
-	}
-
-	[return: NotNullIfNotNull(nameof(value))]
-	public static string? ToSingleLine(this string? value) => value?.Replace("\n", "\\n").Replace("\r", "\\r");
-
-	[return: NotNullIfNotNull(nameof(value))]
-	public static string? ToSingleLineIf(this string? value, bool condition)
-	{
-		if (!condition)
-		{
-			return value;
-		}
-
-		return value?.Replace("\n", "\\n").Replace("\r", "\\r");
-	}
-
-	[return: NotNullIfNotNull(nameof(value))]
-	public static string? TruncateWithEllipsis(this string? value, int maxLength)
-	{
-		if (value is null || value.Length <= maxLength)
-		{
-			return value;
-		}
-
-		const char ellipsis = '\u2026';
-		return $"{value.Substring(0, maxLength)}{ellipsis}";
-	}
-
-	[return: NotNullIfNotNull(nameof(value))]
-	public static string? TruncateWithEllipsisOnWord(this string? value, int maxLength)
-	{
-		if (value is null || value.Length <= maxLength)
-		{
-			return value;
-		}
-
-		int indexOfWordBoundary = value[..maxLength].LastIndexOf(' ');
-		if (indexOfWordBoundary < maxLength * 0.8)
-		{
-			indexOfWordBoundary = maxLength;
-		}
-
-		const char ellipsis = '\u2026';
-		return $"{value.Substring(0, indexOfWordBoundary)}{ellipsis}";
 	}
 }
