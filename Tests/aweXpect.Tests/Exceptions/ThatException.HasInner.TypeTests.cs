732f5d9da08cebe94fa74802cf7fdd090c9b7aeb
Valentin Breuß <vbreuss@gmail.com>
2025-02-14 09:13:15 +0100

    fix: incorrectly named exception expectation (#369)
    Rename "HaveInner" to "HasInner" and consolidate the behavior between generic and type-based expectations.

---
+++ Tests/aweXpect.Tests/Exceptions/ThatException.HasInner.TypeTests.cs
@@ -0,0 +1,107 @@
+﻿namespace aweXpect.Tests;
+
+public sealed partial class ThatException
+{
+	public sealed partial class HasInner
+	{
+		public sealed class TypeTests
+		{
+			[Fact]
+			public async Task WhenInnerExceptionHasCorrectMessageButUnexpectedType_ShouldFail()
+			{
+				Exception subject = new("outer", new Exception("inner"));
+
+				async Task Act()
+					=> await That(subject).HasInner(typeof(CustomException), e => e.HasMessage("inner"));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             has an inner CustomException whose Message is equal to "inner",
+					             but it was an Exception:
+					               inner
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenInnerExceptionHasCorrectTypeAndMessage_ShouldSucceed()
+			{
+				Exception subject = new("outer",
+					new CustomException("inner"));
+
+				async Task Act()
+					=> await That(subject).HasInner(typeof(CustomException), e => e.HasMessage("inner"));
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenInnerExceptionHasCorrectTypeButUnexpectedMessage_ShouldFail()
+			{
+				Exception subject = new("outer",
+					new CustomException("inner"));
+
+				async Task Act()
+					=> await That(subject).HasInner(typeof(CustomException), e => e.HasMessage("some other message"));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             has an inner CustomException whose Message is equal to "some other message",
+					             but it was "inner" which differs at index 0:
+					                ↓ (actual)
+					               "inner"
+					               "some other message"
+					                ↑ (expected)
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenInnerExceptionIsNotOfTheExpectedType_ShouldFail()
+			{
+				Exception subject = new("outer",
+					new Exception("inner"));
+
+				async Task Act()
+					=> await That(subject).HasInner(typeof(CustomException));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             has an inner CustomException,
+					             but it was an Exception:
+					               inner
+					             """);
+			}
+
+
+			[Fact]
+			public async Task WhenInnerExceptionMeetsType_ShouldSucceed()
+			{
+				Exception subject = new("outer",
+					new CustomException("inner"));
+
+				async Task Act()
+					=> await That(subject).HasInner(typeof(CustomException));
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenSubjectIsNull_ShouldFail()
+			{
+				Exception? subject = null;
+
+				async Task Act()
+					=> await That(subject).HasInner(typeof(CustomException));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             has an inner CustomException,
+					             but it was <null>
+					             """);
+			}
+		}
+	}
+}
