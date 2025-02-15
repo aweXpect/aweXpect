e52f157a641055d21e9770c3949776d7fb8135e8
Valentin <valentin.breuss@baur.eu>
2025-02-15 20:45:13 +0100

    Add output

--- Pipeline/Build.Pages.cs
+++ Pipeline/Build.Pages.cs
@@ -7,6 +7,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Nuke.Common;
 using Nuke.Common.IO;
+using Serilog;
 
 namespace Build;
 
@@ -37,30 +38,41 @@ partial class Build
 	async Task DownloadDocsPagesDirectory(string projectName, string directoryName)
 	{
 		AbsolutePath baseDirectory = RootDirectory / "Docs" / "pages" / "docs" / "extensions" / directoryName;
+		Log.Information($"Store documentation from {projectName} under {baseDirectory}:");
 
 		using HttpClient client = new();
 		client.DefaultRequestHeaders.UserAgent.ParseAdd("aweXpect");
 		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GithubToken);
 		HttpResponseMessage response = await client.GetAsync(
 			$"https://api.github.com/repos/aweXpect/{projectName}/contents/Docs/pages");
-		string responseContent = await response.Content.ReadAsStringAsync();
-		JsonDocument jsonDocument = JsonDocument.Parse(responseContent);
-		foreach (JsonElement file in jsonDocument.RootElement.EnumerateArray())
-		{
-			string name = file.GetProperty("name").GetString();
-			HttpResponseMessage fileResponse =
-				await client.GetAsync(
-					$"https://api.github.com/repos/aweXpect/{projectName}/contents/Docs/pages/{name}");
-			string fileResponseContent = await fileResponse.Content.ReadAsStringAsync();
-			using JsonDocument document = JsonDocument.Parse(fileResponseContent);
-			string content = Base64Decode(document.RootElement.GetProperty("content").GetString());
-			await File.WriteAllTextAsync(Path.Combine(baseDirectory, name), content);
-		}
 
+		string responseContent = await response.Content.ReadAsStringAsync();
 		if (!response.IsSuccessStatusCode)
 		{
 			throw new InvalidOperationException(
 				$"Could not find 'Docs/pages/static/js/data.js' in branch '{BenchmarkBranch}': {responseContent}");
+		}
+
+		try
+		{
+			JsonDocument jsonDocument = JsonDocument.Parse(responseContent);
+			foreach (JsonElement file in jsonDocument.RootElement.EnumerateArray())
+			{
+				string name = file.GetProperty("name").GetString();
+				string filePath = Path.Combine(baseDirectory, name);
+				HttpResponseMessage fileResponse =
+					await client.GetAsync(
+						$"https://api.github.com/repos/aweXpect/{projectName}/contents/Docs/pages/{name}");
+				string fileResponseContent = await fileResponse.Content.ReadAsStringAsync();
+				using JsonDocument document = JsonDocument.Parse(fileResponseContent);
+				string content = Base64Decode(document.RootElement.GetProperty("content").GetString());
+				await File.WriteAllTextAsync(filePath, content);
+				Log.Information($"  {name} under {filePath}");
+			}
+		}
+		catch (JsonException e)
+		{
+			Log.Error($"Could not parse JSON: {e.Message}\n{responseContent}");
 		}
 	}
 }
