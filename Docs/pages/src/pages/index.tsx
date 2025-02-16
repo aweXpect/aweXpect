da1f968c659fc1139f484d4e3ab5a4387fe8fb4c
Valentin Breuß <vbreuss@gmail.com>
2025-02-16 04:48:16 +0100

    docs: add "Getting started" button to home page (#383)
    * docs: add "Getting started" button to home page
    * Set button background in dark mode

--- Docs/pages/src/pages/index.tsx
+++ Docs/pages/src/pages/index.tsx
@@ -17,6 +17,13 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+		  <div className={styles.buttons}>
+			  <Link
+				  className="button button--secondary button--lg"
+				  to="/docs/expectations/getting-started">
+				  Getting started
+			  </Link>
+		  </div>
       </div>
     </header>
   );
