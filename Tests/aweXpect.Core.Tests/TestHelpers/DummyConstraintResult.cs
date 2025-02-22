2ae88473e9ef5acba73315571f9f29770bab45b2
Valentin <valentin.breuss@baur.eu>
2025-02-22 11:08:57 +0100

    fix: consider `FurtherProcessingStrategy` in `MappingNode`

--- Tests/aweXpect.Core.Tests/TestHelpers/DummyConstraintResult.cs
+++ Tests/aweXpect.Core.Tests/TestHelpers/DummyConstraintResult.cs
@@ -5,17 +5,32 @@ namespace aweXpect.Core.Tests.TestHelpers;
 
 public class DummyConstraintResult : ConstraintResult
 {
+	private readonly string? _expectationText;
+	private readonly string? _failureText;
+
 	public DummyConstraintResult(Outcome outcome,
-		FurtherProcessingStrategy furtherProcessingStrategy = FurtherProcessingStrategy.Continue)
+		FurtherProcessingStrategy furtherProcessingStrategy = FurtherProcessingStrategy.Continue,
+		string? expectationText = null,
+		string? failureText = null)
 		: base(outcome, furtherProcessingStrategy)
 	{
+		_expectationText = expectationText;
+		_failureText = failureText;
 	}
 
 	public override void AppendExpectation(StringBuilder stringBuilder, string? indentation = null)
 	{
+		if (_expectationText != null)
+		{
+			stringBuilder.Append(_expectationText);
+		}
 	}
 
 	public override void AppendResult(StringBuilder stringBuilder, string? indentation = null)
 	{
+		if (_failureText != null)
+		{
+			stringBuilder.Append(_failureText);
+		}
 	}
 }
