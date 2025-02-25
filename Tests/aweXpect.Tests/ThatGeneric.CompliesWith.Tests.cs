977afeac6e33a93ac6bb327776c82fa9020c973e
Valentin Breuß <vbreuss@gmail.com>
2025-02-25 20:40:41 +0100

    feat: add `CompliesWith` as generic expectation (#412)
    Add a generic expectation `CompliesWith` that allows nesting expectations within one another.
    
    Use case is, that you could store your expecation that you want to apply to in multiple tests in a central location and ensure that all instances "comply with" the stored expectation.

---
+++ Tests/aweXpect.Tests/ThatGeneric.CompliesWith.Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace aweXpect.Tests;
+
+public sealed partial class ThatGeneric
+{
+	public sealed class CompliesWith
+	{
+		public sealed class Tests
+		{
+			[Theory]
+			[InlineData(1, true)]
+			[InlineData(2, false)]
+			public async Task WhenValueIsDifferent_ShouldFail(int expectedValue, bool expectSuccess)
+			{
+				Other subject = new()
+				{
+					Value = 1,
+				};
+
+				async Task Act()
+					=> await That(subject).CompliesWith(x => x.IsEquivalentTo(new
+					{
+						Value = expectedValue,
+					}));
+
+				await That(Act).Throws<XunitException>()
+					.OnlyIf(!expectSuccess)
+					.WithMessage("""
+					             Expected that subject
+					             is equivalent to new
+					             					{
+					             						Value = expectedValue,
+					             					},
+					             but it was Other {
+					               Value = 1
+					             }
+					             """);
+			}
+		}
+	}
+}
--- Tests/aweXpect.Tests/ThatGeneric.DoesNotSatisfy.Tests.cs
+++ Tests/aweXpect.Tests/ThatGeneric.DoesNotSatisfy.Tests.cs
@@ -2,25 +2,25 @@
 
 public sealed partial class ThatGeneric
 {
-	public sealed class Satisfies
+	public sealed class DoesNotSatisfy
 	{
 		public sealed class Tests
 		{
 			[Theory]
 			[InlineData(true)]
 			[InlineData(false)]
-			public async Task ShouldFailWhenPredicateResultIsFalse(bool predicateResult)
+			public async Task ShouldFailWhenPredicateResultIsTrue(bool predicateResult)
 			{
 				Other subject = new();
 
 				async Task Act()
-					=> await That(subject).Satisfies(_ => predicateResult);
+					=> await That(subject).DoesNotSatisfy(_ => predicateResult);
 
 				await That(Act).Throws<XunitException>()
-					.OnlyIf(!predicateResult)
+					.OnlyIf(predicateResult)
 					.WithMessage("""
 					             Expected that subject
-					             satisfy _ => predicateResult,
+					             not satisfy _ => predicateResult,
 					             but it was Other {
 					               Value = 0
 					             }
