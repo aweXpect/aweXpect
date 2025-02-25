485d524cbdd18473b0d2c69f120e80a1ba1d45ac
Valentin Breuß <vbreuss@gmail.com>
2025-02-25 21:22:00 +0100

    feat: add `Within` repeated check for `Satisfies` and `CompliesWith` (#413)
    ## Satisfy
    
    You can verify, that any object satisfies a given predicate:
    
    ```csharp
    object? subject = null;
    
    await Expect.That(subject).Satisfies(x => x == null);
    ```
    
    When the object changes in the background, you can also verify, that it satisfies a condition within a given time period:
    
    ```csharp
    MyClass subject = new() {
    	IsTriggered = false
    };
    // Start a background task that sets `IsTriggered` to true
    
    await Expect.That(subject).Satisfies(x => x.IsTriggered == true).Within(2.Seconds());
    // using aweXpect.Chronology
    ```
    
    ## Comply with
    
    You can verify, that any object complies with an expectation:
    
    ```csharp
    List<int> values = new();
    
    await Expect.That(values).CompliesWith(x => x.IsEmpty());
    ```
    
    When the object changes in the background, you can also verify, that it complies with an expectation within a given time period:
    
    ```csharp
    List<int> values = new();
    // Start a background task that adds items to `values`
    
    await Expect.That(values).CompliesWith(x => x.HasCount().AtLeast(4)).Within(2.Seconds());
    // using aweXpect.Chronology
    ```

--- Source/aweXpect/That/ThatGeneric.CompliesWith.cs
+++ Source/aweXpect/That/ThatGeneric.CompliesWith.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using aweXpect.Core;
 using aweXpect.Core.Constraints;
 using aweXpect.Core.EvaluationContext;
 using aweXpect.Helpers;
+using aweXpect.Options;
 using aweXpect.Results;
 
 namespace aweXpect;
@@ -14,22 +16,28 @@ public static partial class ThatGeneric
 	/// <summary>
 	///     Verifies that the actual value complies with the <paramref name="expectations" />.
 	/// </summary>
-	public static AndOrResult<T, IThat<T>> CompliesWith<T>(this IThat<T> source,
+	public static RepeatedCheckResult<T, IThat<T>> CompliesWith<T>(this IThat<T> source,
 		Action<IThat<T>> expectations)
-		=> new(source.ThatIs().ExpectationBuilder
+	{
+		RepeatedCheckOptions options = new();
+		return new RepeatedCheckResult<T, IThat<T>>(source.ThatIs().ExpectationBuilder
 				.AddConstraint((it, grammar) =>
-					new ComplyWithConstraint<T>(it, expectations)),
-			source);
+					new ComplyWithConstraint<T>(it, expectations, options)),
+			source,
+			options);
+	}
 
 	private readonly struct ComplyWithConstraint<T> : IAsyncContextConstraint<T>
 	{
 		private readonly string _it;
+		private readonly RepeatedCheckOptions _options;
 		private readonly ManualExpectationBuilder<T> _itemExpectationBuilder;
 
 		public ComplyWithConstraint(string it,
-			Action<IThat<T>> expectations)
+			Action<IThat<T>> expectations, RepeatedCheckOptions options)
 		{
 			_it = it;
+			_options = options;
 			_itemExpectationBuilder = new ManualExpectationBuilder<T>();
 			expectations.Invoke(new ThatSubject<T>(_itemExpectationBuilder));
 		}
@@ -45,11 +53,34 @@ public static partial class ThatGeneric
 				return new ConstraintResult.Success<T>(actual, ToString());
 			}
 
+			if (_options.Timeout > TimeSpan.Zero)
+			{
+				Stopwatch? sw = new();
+				sw.Start();
+				do
+				{
+					try
+					{
+						await Task.Delay(_options.Interval.NextCheckInterval(), cancellationToken);
+					}
+					catch (TaskCanceledException)
+					{
+						break;
+					}
+
+					isMatch = await _itemExpectationBuilder.IsMetBy(actual, context, cancellationToken);
+					if (isMatch.Outcome == Outcome.Success)
+					{
+						return new ConstraintResult.Success<T>(actual, ToString());
+					}
+				} while (sw.Elapsed <= _options.Timeout && !cancellationToken.IsCancellationRequested);
+			}
+
 			return new ConstraintResult.Failure(ToString(),
 				$"{_it} was {Formatter.Format(actual)}");
 		}
 
 		public override string ToString()
-			=> $"{_itemExpectationBuilder}";
+			=> $"{_itemExpectationBuilder}{_options}";
 	}
 }
