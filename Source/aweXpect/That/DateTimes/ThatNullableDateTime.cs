f6e32d7a136125a891075f0e8d4d95b2942cbb18
Valentin <valentin.breuss@baur.eu>
2025-03-16 13:09:17 +0100

    Add remaining negations for `DateTime`

--- Source/aweXpect/That/DateTimes/ThatNullableDateTime.cs
+++ Source/aweXpect/That/DateTimes/ThatNullableDateTime.cs
@@ -9,39 +9,4 @@ namespace aweXpect;
 /// <summary>
 ///     Expectations on <see cref="DateTime" />? values.
 /// </summary>
-public static partial class ThatNullableDateTime
-{
-	private sealed class ConditionConstraint(
-		string it,
-		ExpectationGrammars grammars,
-		DateTime? expected,
-		string expectation,
-		Func<DateTime?, DateTime?, TimeSpan, bool> condition,
-		Func<DateTime?, DateTime?, string, string> failureMessageFactory,
-		TimeTolerance tolerance)
-		: ConstraintResult.WithValue<DateTime?>(grammars),
-			IValueConstraint<DateTime?>
-	{
-		public ConstraintResult IsMetBy(DateTime? actual)
-		{
-			Actual = actual;
-			Outcome = condition(actual, expected, tolerance.Tolerance
-			                                      ?? Customize.aweXpect.Settings().DefaultTimeComparisonTolerance.Get())
-				? Outcome.Success
-				: Outcome.Failure;
-			return this;
-		}
-
-		protected override void AppendNormalExpectation(StringBuilder stringBuilder, string? indentation = null)
-			=> stringBuilder.Append(expectation).Append(tolerance);
-
-		protected override void AppendNormalResult(StringBuilder stringBuilder, string? indentation = null)
-			=> stringBuilder.Append(failureMessageFactory(Actual, expected, it));
-
-		protected override void AppendNegatedExpectation(StringBuilder stringBuilder, string? indentation = null)
-			=> throw new NotImplementedException();
-
-		protected override void AppendNegatedResult(StringBuilder stringBuilder, string? indentation = null)
-			=> throw new NotImplementedException();
-	}
-}
+public static partial class ThatNullableDateTime;
--- Tests/aweXpect.Tests/DateTimes/ThatDateTime.IsNotBefore.Tests.cs
+++ Tests/aweXpect.Tests/DateTimes/ThatDateTime.IsNotBefore.Tests.cs
@@ -72,21 +72,15 @@ public sealed partial class ThatDateTime
 			}
 
 			[Fact]
-			public async Task WhenUnexpectedIsNull_ShouldFail()
+			public async Task WhenUnexpectedIsNull_ShouldSucceed()
 			{
 				DateTime subject = CurrentTime();
 				DateTime? unexpected = null;
 
 				async Task Act()
-					=> await That(subject).IsNotBefore(unexpected)
-						.Because("we want to test the failure");
+					=> await That(subject).IsNotBefore(unexpected);
 
-				await That(Act).Throws<XunitException>()
-					.WithMessage($"""
-					              Expected that subject
-					              is not before <null>, because we want to test the failure,
-					              but it was {Formatter.Format(subject)}
-					              """);
+				await That(Act).DoesNotThrow();
 			}
 
 			[Fact]
