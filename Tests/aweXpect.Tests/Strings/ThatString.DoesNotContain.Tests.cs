5e32b397ef1fb107b1f7cf1082ab5faf1ecd2b20
Valentin Breuß <vbreuss@gmail.com>
2025-05-04 22:15:54 +0200

    fix: correctly handle `null` in string `Contains`
    When the expected (or unexpected) string is `null` in string `Contains`/`DoesNotContain` expectation, provide a correct error message.

--- Tests/aweXpect.Tests/Strings/ThatString.DoesNotContain.Tests.cs
+++ Tests/aweXpect.Tests/Strings/ThatString.DoesNotContain.Tests.cs
@@ -11,10 +11,10 @@ public sealed partial class ThatString
 			{
 				string subject =
 					"In this text in between the word an investigator should find the word 'IN' multiple times.";
-				string expected = "INVESTIGATOR";
+				string unexpected = "INVESTIGATOR";
 
 				async Task Act()
-					=> await That(subject).DoesNotContain(expected).IgnoringCase();
+					=> await That(subject).DoesNotContain(unexpected).IgnoringCase();
 
 				await That(Act).Throws<XunitException>()
 					.WithMessage("""
@@ -29,10 +29,10 @@ public sealed partial class ThatString
 			{
 				string subject =
 					"In this text in between the word an investigator should find the word 'IN' multiple times.";
-				string expected = "InvEstIgAtOr";
+				string unexpected = "InvEstIgAtOr";
 
 				async Task Act()
-					=> await That(subject).DoesNotContain(expected)
+					=> await That(subject).DoesNotContain(unexpected)
 						.Using(new IgnoreCaseForVocalsComparer());
 
 				await That(Act).Throws<XunitException>()
@@ -41,35 +41,6 @@ public sealed partial class ThatString
 					             does not contain "InvEstIgAtOr" using IgnoreCaseForVocalsComparer,
 					             but it contained it 1 times in "In this text in between the word an investigator should find the word 'IN' multiple times."
 					             """);
-			}
-
-			[Fact]
-			public async Task WhenExpectedStringIsContained_ShouldFail()
-			{
-				string subject = "some text";
-				string expected = "me";
-
-				async Task Act()
-					=> await That(subject).DoesNotContain(expected);
-
-				await That(Act).Throws<XunitException>()
-					.WithMessage("""
-					             Expected that subject
-					             does not contain "me",
-					             but it contained it 1 times in "some text"
-					             """);
-			}
-
-			[Fact]
-			public async Task WhenExpectedStringIsNotContained_ShouldSucceed()
-			{
-				string subject = "some text";
-				string expected = "not";
-
-				async Task Act()
-					=> await That(subject).DoesNotContain(expected);
-
-				await That(Act).DoesNotThrow();
 			}
 
 			[Fact]
@@ -84,6 +55,52 @@ public sealed partial class ThatString
 				await That(Act).Throws<ArgumentException>()
 					.WithMessage("The 'unexpected' string cannot be empty.").AsPrefix().And
 					.WithParamName("unexpected");
+			}
+
+			[Fact]
+			public async Task WhenUnexpectedIsNull_ShouldFail()
+			{
+				string subject = "some text";
+				string? unexpected = null;
+
+				async Task Act()
+					=> await That(subject).DoesNotContain(unexpected!);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             does not contain <null>,
+					             but "some text" cannot be validated against <null>
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenUnexpectedStringIsContained_ShouldFail()
+			{
+				string subject = "some text";
+				string unexpected = "me";
+
+				async Task Act()
+					=> await That(subject).DoesNotContain(unexpected);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             does not contain "me",
+					             but it contained it 1 times in "some text"
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenUnexpectedStringIsNotContained_ShouldSucceed()
+			{
+				string subject = "some text";
+				string unexpected = "not";
+
+				async Task Act()
+					=> await That(subject).DoesNotContain(unexpected);
+
+				await That(Act).DoesNotThrow();
 			}
 		}
 	}
