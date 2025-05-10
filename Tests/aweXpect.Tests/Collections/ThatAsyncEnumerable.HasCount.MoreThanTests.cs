dfd692490260858bfb187eb63a6b7f62a9c5155b
Valentin Breuß <vbreuss@gmail.com>
2025-05-10 06:28:44 +0200

    feat: add `LessThan`/`MoreThan` for collection `HasCount`

---
+++ Tests/aweXpect.Tests/Collections/ThatAsyncEnumerable.HasCount.MoreThanTests.cs
@@ -0,0 +1,96 @@
+﻿#if NET8_0_OR_GREATER
+using System.Collections.Generic;
+using System.Threading;
+
+// ReSharper disable PossibleMultipleEnumeration
+
+namespace aweXpect.Tests;
+
+public sealed partial class ThatAsyncEnumerable
+{
+	public sealed partial class HasCount
+	{
+		public sealed class MoreThanTests
+		{
+			[Fact]
+			public async Task ConsidersCancellationToken()
+			{
+				using CancellationTokenSource cts = new();
+				CancellationToken token = cts.Token;
+				IAsyncEnumerable<int> subject =
+					GetCancellingAsyncEnumerable(4, cts, CancellationToken.None);
+
+				async Task Act()
+					=> await That(subject).HasCount().MoreThan(6)
+						.WithCancellation(token);
+
+				await That(Act).Throws<InconclusiveException>()
+					.WithMessage("""
+					             Expected that subject
+					             has more than 6 items,
+					             but could not verify, because it was already cancelled
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenEnumerableContainsMatchingItems_ShouldFail()
+			{
+				IAsyncEnumerable<int> subject = ToAsyncEnumerable([1, 2, 3,]);
+
+				async Task Act()
+					=> await That(subject).HasCount().MoreThan(3);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             has more than 3 items,
+					             but found only 3
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenEnumerableContainsTooFewItems_ShouldFail()
+			{
+				IAsyncEnumerable<int> subject = ToAsyncEnumerable([1, 2, 3,]);
+
+				async Task Act()
+					=> await That(subject).HasCount().MoreThan(4);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             has more than 4 items,
+					             but found only 3
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenEnumerableContainsTooManyItems_ShouldSucceed()
+			{
+				IAsyncEnumerable<int> subject = ToAsyncEnumerable([1, 2, 3,]);
+
+				async Task Act()
+					=> await That(subject).HasCount().MoreThan(2);
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenSubjectIsNull_ShouldFail()
+			{
+				IAsyncEnumerable<int>? subject = null;
+
+				async Task Act()
+					=> await That(subject).HasCount().MoreThan(2);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             has more than 2 items,
+					             but it was <null>
+					             """);
+			}
+		}
+	}
+}
+#endif
