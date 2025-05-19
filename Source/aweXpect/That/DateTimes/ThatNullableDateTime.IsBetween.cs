0fb5973e131cd85352c9425582e951661e753782
Valentin <valentin.breuss@baur.eu>
2025-05-19 11:16:38 +0200

    feat: add `IsBetween`/`IsNotBetween` for `DateTime` and `DateTimeOffset`
    ## Between
    
    You can verify that the `DateTime` or `DateTimeOffset` is between two values:
    
    ```csharp
    DateTime subject1 = DateTime.Now;
    
    await Expect.That(subject1).IsBetween(new DateTime(2024, 1, 1)).And(new DateTime(2123, 12, 31));
    
    DateTimeOffset subject2 = DateTimeOffset.Now;
    
    await Expect.That(subject2)
        .IsBetween(new DateTimeOffset(2024, 1, 1, 0, 0, 0, TimeSpan.FromHours(2)))
        .And(new DateTimeOffset(2124, 12, 31, 23, 59, 59, TimeSpan.FromHours(2)));
    ```
    
    You can also specify a tolerance:
    
    ```csharp
    DateTime subject = DateTime.Now;
    
    await Expect.That(subject).IsBetween(DateTime.Today).And(DateTime.Now).Within(TimeSpan.FromSeconds(1))
      .Because("it should have taken less than one second");
    ```

---
+++ Source/aweXpect/That/DateTimes/ThatNullableDateTime.IsBetween.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using aweXpect.Core;
+using aweXpect.Core.Constraints;
+using aweXpect.Customization;
+using aweXpect.Helpers;
+using aweXpect.Options;
+using aweXpect.Results;
+
+namespace aweXpect;
+
+public static partial class ThatNullableDateTime
+{
+	/// <summary>
+	///     Verifies that the subject is between the <paramref name="minimum" />…
+	/// </summary>
+	public static BetweenResult<TimeToleranceResult<DateTime?, IThat<DateTime?>>, DateTime?> IsBetween(
+		this IThat<DateTime?> source,
+		DateTime? minimum)
+	{
+		TimeTolerance tolerance = new();
+		return new BetweenResult<TimeToleranceResult<DateTime?, IThat<DateTime?>>, DateTime?>(maximum
+			=> new TimeToleranceResult<DateTime?, IThat<DateTime?>>(
+				source.Get().ExpectationBuilder.AddConstraint((it, grammars) =>
+					new IsBetweenConstraint(it, grammars, minimum, maximum, tolerance)),
+				source,
+				tolerance));
+	}
+
+	/// <summary>
+	///     Verifies that the subject is not between the <paramref name="minimum" />…
+	/// </summary>
+	public static BetweenResult<TimeToleranceResult<DateTime?, IThat<DateTime?>>, DateTime?> IsNotBetween(
+		this IThat<DateTime?> source,
+		DateTime? minimum)
+	{
+		TimeTolerance tolerance = new();
+		return new BetweenResult<TimeToleranceResult<DateTime?, IThat<DateTime?>>, DateTime?>(maximum
+			=> new TimeToleranceResult<DateTime?, IThat<DateTime?>>(
+				source.Get().ExpectationBuilder.AddConstraint((it, grammars) =>
+					new IsBetweenConstraint(it, grammars, minimum, maximum, tolerance).Invert()),
+				source,
+				tolerance));
+	}
+
+	private sealed class IsBetweenConstraint(
+		string it,
+		ExpectationGrammars grammars,
+		DateTime? minimum,
+		DateTime? maximum,
+		TimeTolerance tolerance)
+		: ConstraintResult.WithNotNullValue<DateTime?>(it, grammars),
+			IValueConstraint<DateTime?>
+	{
+		public ConstraintResult IsMetBy(DateTime? actual)
+		{
+			Actual = actual;
+			if (actual is null && minimum is null && maximum is null)
+			{
+				Outcome = Outcome.Success;
+			}
+			else if (actual is null || minimum is null || maximum is null)
+			{
+				Outcome = IsNegated ? Outcome.Success : Outcome.Failure;
+			}
+			else
+			{
+				TimeSpan timeTolerance = tolerance.Tolerance
+				                         ?? Customize.aweXpect.Settings().DefaultTimeComparisonTolerance.Get();
+				if (IsNegated)
+				{
+					timeTolerance = timeTolerance.Negate();
+				}
+
+				Outcome = actual.Value.Add(timeTolerance) >= minimum && actual.Value.Add(timeTolerance.Negate()) <= maximum
+					? Outcome.Success
+					: Outcome.Failure;
+			}
+
+			return this;
+		}
+
+		protected override void AppendNormalExpectation(StringBuilder stringBuilder, string? indentation = null)
+		{
+			stringBuilder.Append("is between ");
+			Formatter.Format(stringBuilder, minimum);
+			stringBuilder.Append(" and ");
+			Formatter.Format(stringBuilder, maximum);
+			stringBuilder.Append(tolerance);
+		}
+
+		protected override void AppendNormalResult(StringBuilder stringBuilder, string? indentation = null)
+		{
+			stringBuilder.Append(It).Append(" was ");
+			Formatter.Format(stringBuilder, Actual);
+		}
+
+		protected override void AppendNegatedExpectation(StringBuilder stringBuilder, string? indentation = null)
+		{
+			stringBuilder.Append("is not between ");
+			Formatter.Format(stringBuilder, minimum);
+			stringBuilder.Append(" and ");
+			Formatter.Format(stringBuilder, maximum);
+			stringBuilder.Append(tolerance);
+		}
+
+		protected override void AppendNegatedResult(StringBuilder stringBuilder, string? indentation = null)
+			=> AppendNormalResult(stringBuilder, indentation);
+	}
+}
---
+++ Tests/aweXpect.Tests/DateTimeOffsets/ThatDateTimeOffset.IsNotBetween.Tests.cs
@@ -0,0 +1,259 @@
+﻿namespace aweXpect.Tests;
+
+public sealed partial class ThatDateTimeOffset
+{
+	public sealed class IsNotBetween
+	{
+		public sealed class Tests
+		{
+			[Fact]
+			public async Task WhenMaximumIsNull_ShouldFail()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset? minimum = subject;
+				DateTimeOffset? maximum = null;
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and <null>,
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Fact]
+			public async Task WhenMinimumIsNull_ShouldFail()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset? minimum = null;
+				DateTimeOffset? maximum = subject;
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between <null> and {Formatter.Format(maximum)},
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Fact]
+			public async Task WhenSubjectAndMaximumAreMaxValue_ShouldFail()
+			{
+				DateTimeOffset subject = DateTimeOffset.MaxValue;
+				DateTimeOffset minimum = CurrentTime();
+				DateTimeOffset maximum = DateTimeOffset.MaxValue;
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and {Formatter.Format(maximum)},
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Fact]
+			public async Task WhenSubjectAndMinimumAreMinValue_ShouldFail()
+			{
+				DateTimeOffset subject = DateTimeOffset.MinValue;
+				DateTimeOffset minimum = DateTimeOffset.MinValue;
+				DateTimeOffset maximum = CurrentTime();
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and {Formatter.Format(maximum)},
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Fact]
+			public async Task WhenSubjectIsNotBetweenMinimumAndMaximum_ShouldFail()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset minimum = EarlierTime();
+				DateTimeOffset maximum = LaterTime();
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and {Formatter.Format(maximum)},
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Fact]
+			public async Task WhenSubjectIsEarlierThanMinimum_ShouldSucceed()
+			{
+				DateTimeOffset subject = EarlierTime();
+				DateTimeOffset minimum = CurrentTime();
+				DateTimeOffset maximum = LaterTime();
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenSubjectIsLaterThanMaximum_ShouldSucceed()
+			{
+				DateTimeOffset subject = LaterTime();
+				DateTimeOffset minimum = EarlierTime();
+				DateTimeOffset maximum = CurrentTime();
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenSubjectIsSameAsMaximum_ShouldFail()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset minimum = EarlierTime();
+				DateTimeOffset maximum = subject;
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and {Formatter.Format(maximum)},
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Fact]
+			public async Task WhenSubjectIsSameAsMinimum_ShouldFail()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset minimum = subject;
+				DateTimeOffset maximum = LaterTime();
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and {Formatter.Format(maximum)},
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+		}
+
+		public sealed class WithinTests
+		{
+			[Fact]
+			public async Task WhenMaximumValueIsOutsideTheTolerance_ShouldSucceed()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset minimum = DateTimeOffset.MinValue;
+				DateTimeOffset maximum = LaterTime(2);
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum)
+						.Within(3.Seconds());
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenMinimumValueIsOutsideTheTolerance_ShouldSucceed()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset minimum = EarlierTime(2);
+				DateTimeOffset maximum = DateTimeOffset.MaxValue;
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum)
+						.Within(3.Seconds());
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenNullableMaximumValueIsOutsideTheTolerance_ShouldSucceed()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset minimum = DateTimeOffset.MinValue;
+				DateTimeOffset? maximum = LaterTime(2);
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum)
+						.Within(3.Seconds());
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenNullableMinimumValueIsOutsideTheTolerance_ShouldSucceed()
+			{
+				DateTimeOffset subject = CurrentTime();
+				DateTimeOffset? minimum = EarlierTime(2);
+				DateTimeOffset maximum = DateTimeOffset.MaxValue;
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum)
+						.Within(3.Seconds());
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenValueIsWithinTheMaximumTolerance_ShouldFail()
+			{
+				DateTimeOffset subject = EarlierTime(3);
+				DateTimeOffset minimum = DateTimeOffset.MinValue;
+				DateTimeOffset maximum = CurrentTime();
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum)
+						.Within(3.Seconds());
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and {Formatter.Format(maximum)} ± 0:03,
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Fact]
+			public async Task WhenValueIsWithinTheMinimumTolerance_ShouldFail()
+			{
+				DateTimeOffset subject = LaterTime(3);
+				DateTimeOffset minimum = CurrentTime();
+				DateTimeOffset maximum = DateTimeOffset.MaxValue;
+
+				async Task Act()
+					=> await That(subject).IsNotBetween(minimum).And(maximum)
+						.Within(3.Seconds());
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not between {Formatter.Format(minimum)} and {Formatter.Format(maximum)} ± 0:03,
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+		}
+	}
+}
