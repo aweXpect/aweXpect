c67af14dc95b5e55789c5481abeb4f989a5b9044
Valentin Breuß <vbreuss@gmail.com>
2025-07-28 19:57:57 +0200

    feat: add `IsParsableInto` for strings

---
+++ Tests/aweXpect.Tests/Strings/ThatString.IsNotParsableInto.Tests.cs
@@ -0,0 +1,70 @@
+﻿#if NET8_0_OR_GREATER
+using System.Globalization;
+
+namespace aweXpect.Tests;
+
+public sealed partial class ThatString
+{
+	public class IsNotParsableInto
+	{
+		public sealed class Tests
+		{
+			[Fact]
+			public async Task WhenNull_ShouldSucceed()
+			{
+				string? subject = null;
+
+				async Task Act()
+					=> await That(subject).IsNotParsableInto<int>();
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenStringIsNotParsable_ShouldSucceed()
+			{
+				string subject = "abc";
+
+				async Task Act()
+					=> await That(subject).IsNotParsableInto<int>();
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenStringIsParsable_ShouldFail()
+			{
+				string subject = "42";
+
+				async Task Act()
+					=> await That(subject).IsNotParsableInto<int>();
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is not parsable into int,
+					             but it was
+					             """);
+			}
+
+			[Theory]
+			[InlineData("12,34", "de-AT")]
+			[InlineData("12.34", "en-US")]
+			public async Task WithFormatProvider_ShouldBeUsed(string subject, string cultureName)
+			{
+				IFormatProvider formatProvider = new CultureInfo(cultureName);
+
+				async Task Act()
+					=> await That(subject).IsNotParsableInto<decimal>(formatProvider);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not parsable into decimal using {cultureName},
+					              but it was
+					              """);
+			}
+		}
+	}
+}
+#endif
---
+++ Tests/aweXpect.Tests/Strings/ThatString.IsParsableInto.Tests.cs
@@ -0,0 +1,147 @@
+﻿#if NET8_0_OR_GREATER
+using System.Globalization;
+
+namespace aweXpect.Tests;
+
+public sealed partial class ThatString
+{
+	public class IsParsableInto
+	{
+		public sealed class Tests
+		{
+			[Fact]
+			public async Task WhenNull_ShouldFail()
+			{
+				string? subject = null;
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<int>().Because("null should fail");
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is parsable into int, because null should fail,
+					             but it was <null>
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenStringIsNotParsable_ShouldFail()
+			{
+				string subject = "abc";
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<int>();
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is parsable into int,
+					             but it was not because the input string 'abc' was not in a correct format
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenStringIsParsable_ShouldSucceed()
+			{
+				string subject = "42";
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<int>();
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Theory]
+			[InlineData("12.34", "de-AT")]
+			[InlineData("12.34,0", "en-US")]
+			public async Task WithFormatProvider_WhenFormatDoesNotMatch_ShouldFail(string subject, string cultureName)
+			{
+				IFormatProvider formatProvider = new CultureInfo(cultureName);
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<decimal>(formatProvider);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is parsable into decimal using {cultureName},
+					              but it was not because the input string '{subject}' was not in a correct format
+					              """);
+			}
+
+			[Theory]
+			[InlineData("12,34", "de-AT")]
+			[InlineData("12.34", "en-US")]
+			public async Task WithFormatProvider_WhenFormatMatches_ShouldSucceed(string subject, string cultureName)
+			{
+				IFormatProvider formatProvider = new CultureInfo(cultureName);
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<decimal>(formatProvider);
+
+				await That(Act).DoesNotThrow();
+			}
+		}
+
+		public sealed class WhichTests
+		{
+			[Fact]
+			public async Task WhenNull_ShouldFail()
+			{
+				string? subject = null;
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<int>().Which.IsGreaterThan(2).And.IsLessThan(3);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is parsable into int which is greater than 2 and is less than 3,
+					             but it was <null>
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenStringIsNotParsable_ShouldFail()
+			{
+				string subject = "abc";
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<TimeSpan>().Which.IsLessThan(10.Seconds());
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is parsable into TimeSpan which is less than 0:10,
+					             but it was not because string 'abc' was not recognized as a valid TimeSpan
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenStringIsParsable_ShouldSucceed()
+			{
+				string subject = "42";
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<int>().Which.IsBetween(41).And(43);
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Theory]
+			[InlineData("12,34", "de-AT")]
+			[InlineData("12.34", "en-US")]
+			public async Task WithFormatProvider_ShouldBeUsed(string subject, string cultureName)
+			{
+				IFormatProvider formatProvider = new CultureInfo(cultureName);
+
+				async Task Act()
+					=> await That(subject).IsParsableInto<decimal>(formatProvider).Which.IsEqualTo(12.34M);
+
+				await That(Act).DoesNotThrow();
+			}
+		}
+	}
+}
+#endif
