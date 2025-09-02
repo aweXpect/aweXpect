8904736515832f91713cc9f5d66cf1f81e44fed5
Valentin Breuß <vbreuss@gmail.com>
2025-09-02 14:50:48 +0200

    Add tests for `AndOrResult`

--- Tests/aweXpect.Core.Tests/Results/AndOrResultTests.cs
+++ Tests/aweXpect.Core.Tests/Results/AndOrResultTests.cs
@@ -5,13 +5,13 @@ using aweXpect.Results;
 
 namespace aweXpect.Core.Tests.Results;
 
-public class AndResultTests
+public class AndOrResultTests
 {
 	[Fact]
 	public async Task And_ShouldReturnSubject()
 	{
 		int subject = 1;
-		AndResult<IThat<int>> sut = CreateSut(subject);
+		AndOrResult<IThat<int>> sut = CreateSut(subject);
 
 		IThat<int> result = sut.And;
 
@@ -26,7 +26,7 @@ public class AndResultTests
 	public async Task Generic_And_ShouldReturnSubject()
 	{
 		int subject = 1;
-		AndResult<int, IThat<int>> sut = CreateGenericSut(subject);
+		AndOrResult<int, IThat<int>> sut = CreateGenericSut(subject);
 
 		IThat<int> result = sut.And;
 
@@ -38,10 +38,25 @@ public class AndResultTests
 	}
 
 	[Fact]
+	public async Task Generic_Or_ShouldReturnSubject()
+	{
+		int subject = 1;
+		AndOrResult<int, IThat<int>> sut = CreateGenericSut(subject);
+
+		IThat<int> result = sut.Or;
+
+		async Task Act()
+			=> await result.IsEqualTo(1);
+
+		await That(Act).DoesNotThrow();
+		await That(result.Get().ExpectationBuilder.GetRootNode()).Is<OrNode>();
+	}
+
+	[Fact]
 	public async Task Generic_ShouldBeAwaitable()
 	{
 		int subject = 1;
-		AndResult<int, IThat<int>> sut = CreateGenericSut(subject);
+		AndOrResult<int, IThat<int>> sut = CreateGenericSut(subject);
 
 		int result = await sut;
 
@@ -49,32 +64,47 @@ public class AndResultTests
 	}
 
 	[Fact]
+	public async Task Or_ShouldReturnSubject()
+	{
+		int subject = 1;
+		AndOrResult<IThat<int>> sut = CreateSut(subject);
+
+		IThat<int> result = sut.Or;
+
+		async Task Act()
+			=> await result.IsEqualTo(1);
+
+		await That(Act).DoesNotThrow();
+		await That(result.Get().ExpectationBuilder.GetRootNode()).Is<OrNode>();
+	}
+
+	[Fact]
 	public async Task ShouldBeAwaitable()
 	{
 		int subject = 1;
-		AndResult<IThat<int>> sut = CreateSut(subject);
+		AndOrResult<IThat<int>> sut = CreateSut(subject);
 
 		async Task Act() => await sut;
 
 		await That(Act).DoesNotThrow();
 	}
 
-	private static AndResult<IThat<T>> CreateSut<T>(T subject)
+	private static AndOrResult<IThat<T>> CreateSut<T>(T subject)
 	{
 #pragma warning disable aweXpect0001
 		IThat<T> source = That(subject);
 #pragma warning restore aweXpect0001
-		return new AndResult<IThat<T>>(source.Get().ExpectationBuilder.AddConstraint((it, _)
+		return new AndOrResult<IThat<T>>(source.Get().ExpectationBuilder.AddConstraint((it, _)
 				=> new DummyConstraint(it)),
 			source);
 	}
 
-	private static AndResult<T, IThat<T>> CreateGenericSut<T>(T subject)
+	private static AndOrResult<T, IThat<T>> CreateGenericSut<T>(T subject)
 	{
 #pragma warning disable aweXpect0001
 		IThat<T> source = That(subject);
 #pragma warning restore aweXpect0001
-		return new AndResult<T, IThat<T>>(source.Get().ExpectationBuilder.AddConstraint((it, _)
+		return new AndOrResult<T, IThat<T>>(source.Get().ExpectationBuilder.AddConstraint((it, _)
 				=> new DummyConstraint(it)),
 			source);
 	}
