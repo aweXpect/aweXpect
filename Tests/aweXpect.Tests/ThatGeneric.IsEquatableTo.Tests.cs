df9223447853095d4fd309fd3aff796e30d1ed62
Valentin Breuß <vbreuss@gmail.com>
2025-09-02 11:18:40 +0200

    feat: add `Is{Not}EquatableTo` (#745)
    This PR adds new assertion methods `IsEquatableTo` and `IsNotEquatableTo` to the aweXpect assertion library for testing equality using the `IEquatable<T>` interface.

---
+++ Tests/aweXpect.Tests/ThatGeneric.IsEquatableTo.Tests.cs
@@ -0,0 +1,150 @@
+﻿namespace aweXpect.Tests;
+
+public sealed partial class ThatGeneric
+{
+	public sealed class IsEquatableTo
+	{
+		public sealed class Tests
+		{
+			[Fact]
+			public async Task WhenComparingToMatchingLong_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).IsEquatableTo(1L);
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenComparingToMatchingWrapper_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(1);
+
+				async Task Act()
+					=> await That(subject).IsEquatableTo(expected);
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingLong_ShouldFail()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).IsEquatableTo(2L);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is equatable to 2,
+					             but it was ThatGeneric.IsEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingWrapper_ShouldFail()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(3);
+
+				async Task Act()
+					=> await That(subject).IsEquatableTo(expected);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is equatable to ThatGeneric.IsEquatableTo.Wrapper {
+					               Value = 3
+					             },
+					             but it was ThatGeneric.IsEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+		}
+
+		public sealed class NegatedTests
+		{
+			[Fact]
+			public async Task WhenComparingToMatchingLong_ShouldFail()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsEquatableTo(1L));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is not equatable to 1,
+					             but it was in ThatGeneric.IsEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenComparingToMatchingWrapper_ShouldFail()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(1);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsEquatableTo(expected));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is not equatable to ThatGeneric.IsEquatableTo.Wrapper {
+					               Value = 1
+					             },
+					             but it was in ThatGeneric.IsEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingLong_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsEquatableTo(2L));
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingWrapper_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(3);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsEquatableTo(expected));
+
+				await That(Act).DoesNotThrow();
+			}
+		}
+
+		private readonly struct Wrapper(long value)
+			: IEquatable<Wrapper>,
+				IEquatable<long>
+		{
+			public long Value { get; } = value;
+
+			public bool Equals(Wrapper other)
+				=> Value == other.Value;
+
+			public bool Equals(long other)
+				=> Value == other;
+		}
+	}
+}
---
+++ Tests/aweXpect.Tests/ThatGeneric.IsNotEquatableTo.Tests.cs
@@ -0,0 +1,150 @@
+﻿namespace aweXpect.Tests;
+
+public sealed partial class ThatGeneric
+{
+	public sealed class IsNotEquatableTo
+	{
+		public sealed class Tests
+		{
+			[Fact]
+			public async Task WhenComparingToMatchingLong_ShouldFail()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).IsNotEquatableTo(1L);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is not equatable to 1,
+					             but it was in ThatGeneric.IsNotEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenComparingToMatchingWrapper_ShouldFail()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(1);
+
+				async Task Act()
+					=> await That(subject).IsNotEquatableTo(expected);
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is not equatable to ThatGeneric.IsNotEquatableTo.Wrapper {
+					               Value = 1
+					             },
+					             but it was in ThatGeneric.IsNotEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingLong_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).IsNotEquatableTo(2L);
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingWrapper_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(3);
+
+				async Task Act()
+					=> await That(subject).IsNotEquatableTo(expected);
+
+				await That(Act).DoesNotThrow();
+			}
+		}
+
+		public sealed class NegatedTests
+		{
+			[Fact]
+			public async Task WhenComparingToMatchingLong_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsNotEquatableTo(1L));
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenComparingToMatchingWrapper_ShouldSucceed()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(1);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsNotEquatableTo(expected));
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingLong_ShouldFail()
+			{
+				Wrapper subject = new(1);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsNotEquatableTo(2L));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is equatable to 2,
+					             but it was ThatGeneric.IsNotEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+
+			[Fact]
+			public async Task WhenComparingToNotMatchingWrapper_ShouldFail()
+			{
+				Wrapper subject = new(1);
+				Wrapper expected = new(3);
+
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsNotEquatableTo(expected));
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage("""
+					             Expected that subject
+					             is equatable to ThatGeneric.IsNotEquatableTo.Wrapper {
+					               Value = 3
+					             },
+					             but it was ThatGeneric.IsNotEquatableTo.Wrapper {
+					               Value = 1
+					             }
+					             """);
+			}
+		}
+
+		private readonly struct Wrapper(long value)
+			: IEquatable<Wrapper>,
+				IEquatable<long>
+		{
+			public long Value { get; } = value;
+
+			public bool Equals(Wrapper other)
+				=> Value == other.Value;
+
+			public bool Equals(long other)
+				=> Value == other;
+		}
+	}
+}
