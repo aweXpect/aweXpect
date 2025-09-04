ee3aff4cfbb54d5ef0ed3f09e919ad776ba45bda
Valentin Breuß <vbreuss@gmail.com>
2025-09-04 12:46:51 +0200

    Add test for RepeatedCheckOptions

--- Tests/aweXpect.Internal.Tests/Options/RepeatedCheckOptionsTests.cs
+++ Tests/aweXpect.Internal.Tests/Options/RepeatedCheckOptionsTests.cs
@@ -1,4 +1,5 @@
-﻿using aweXpect.Options;
+﻿using aweXpect.Customization;
+using aweXpect.Options;
 using FluentAssertions.Extensions;
 
 namespace aweXpect.Internal.Tests.Options;
@@ -11,5 +12,24 @@ public class RepeatedCheckOptionsTests
 		TimeSpan result = RepeatedCheckOptions.DefaultInterval;
 
 		await That(result).IsEqualTo(100.Milliseconds());
+	}
+
+	[Fact]
+	public async Task Interval_ShouldBeReadOnlyOnceFromCustomization()
+	{
+		RepeatedCheckOptions sut = new();
+		ICheckInterval interval1, interval2;
+		using (Customize.aweXpect.Settings().DefaultCheckInterval.Set(103.Milliseconds()))
+		{
+			interval1 = sut.Interval;
+		}
+
+		using (Customize.aweXpect.Settings().DefaultCheckInterval.Set(107.Milliseconds()))
+		{
+			interval2 = sut.Interval;
+		}
+
+		await That(interval1.NextCheckInterval()).IsEqualTo(103.Milliseconds());
+		await That(interval1.NextCheckInterval()).IsEqualTo(interval2.NextCheckInterval());
 	}
 }
