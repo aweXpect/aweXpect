372c087c1806c2fd85e1857e3477d68456cdb416
Valentin Breuß <vbreuss@gmail.com>
2025-10-18 17:34:18 +0200

    Fix failing tests

--- Tests/Frameworks/aweXpect.Frameworks.MsTest4.Tests/MsTest4FrameworkTests.cs
+++ Tests/Frameworks/aweXpect.Frameworks.MsTest4.Tests/MsTest4FrameworkTests.cs
@@ -1,8 +1,10 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 [assembly: Parallelize]
 
 namespace aweXpect.Frameworks.MsTest4.Tests;
+
 [TestClass]
 public sealed class MsTestFrameworkTests
 {
